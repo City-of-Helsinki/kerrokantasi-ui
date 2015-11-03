06a7a1fde2a8442e16e11d081d3b1becf8d3f71d
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-03 11:36:58 +0200

    Info: Fix eslint complaint

--- src/views/Info/index.js
+++ src/views/Info/index.js
@@ -5,9 +5,9 @@ import {connect} from 'react-redux';
 class Info extends React.Component {
   render() {
     const content = {'fi': finnishContent}[this.props.language];
-    return <div className="container">
+    return (<div className="container">
       <div dangerouslySetInnerHTML={{__html: content || "Content not available in current language"}} />
-    </div>;
+    </div>);
   }
 }
 
