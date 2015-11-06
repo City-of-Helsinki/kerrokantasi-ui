07de25dffa5939ae73ec958255e86226cd9b4ddd
Raphaël Stefanini <raphael.stefanini@anders.fi>
2015-11-06 10:50:13 +0200

    Add HearingImageList Component

--- src/views/Hearing/index.js
+++ src/views/Hearing/index.js
@@ -6,12 +6,13 @@ import Button from 'react-bootstrap/lib/Button';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
 import Col from 'react-bootstrap/lib/Col';
 import Label from 'react-bootstrap/lib/Label';
-import {injectIntl, FormattedMessage} from 'react-intl';
+import {injectIntl, FormattedMessage, FormattedRelative} from 'react-intl';
 import {fetchHearing} from 'actions';
 import Comment from './Comment';
 import Scenario from './Scenario';
 import LabelList from 'src/components/LabelList';
 import OverviewMap from 'src/components/OverviewMap';
+import HearingImageList from 'src/components/HearingImageList';
 
 class Hearing extends React.Component {
   componentDidMount() {
@@ -53,7 +54,7 @@ class Hearing extends React.Component {
       <Col xs={6} sm={3}>
         <div>
           <h4><FormattedMessage id="timetable"/></h4>
-          <i className="fa fa-clock-o"></i> <FormattedMessage id="closing"/> {data.close_at}
+          <i className="fa fa-clock-o"></i> <FormattedMessage id="closing"/> <FormattedRelative value={data.close_at}/>
         </div>
         <div>
           <h4><FormattedMessage id="table-of-content"/></h4>
@@ -74,10 +75,7 @@ class Hearing extends React.Component {
           <LabelList labels={data.labels}/>
           <div>
             <h1>{data.heading}</h1>
-            <img width="100%" src="/assets/carousel.png"/>
-            <div className="image-caption">
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
+            <HearingImageList images={data.images}/>
             <div className="hearing-abstract" dangerouslySetInnerHTML={{__html: data.abstract}}/>
           </div>
           <div dangerouslySetInnerHTML={{__html: data.content}} />
