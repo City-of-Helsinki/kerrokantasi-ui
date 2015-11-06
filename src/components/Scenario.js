29bdb3bdd357cb6f570543179b0e2cfe3c127813
Raphaël Stefanini <raphael.stefanini@anders.fi>
2015-11-06 15:38:41 +0200

    Use scenario titles

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -14,12 +14,12 @@ export default class Scenario extends React.Component {
     const {data} = this.props;
     if (this.state.collapsed) {
       return (<div className="hearing-scenario">
-        <h3 className="scenario-title" onClick={this.toogle.bind(this)}><i className="fa fa-chevron-right"></i> {'Scenario Name'}</h3>
+        <h3 className="scenario-title" onClick={this.toogle.bind(this)}><i className="fa fa-chevron-right"></i> {data.title}</h3>
         <hr/>
       </div>);
     }
     return (<div className="hearing-scenario">
-      <h3 className="scenario-title" onClick={this.toogle.bind(this)}><i className="fa fa-chevron-down"></i> {'Scenario Name'}</h3>
+      <h3 className="scenario-title" onClick={this.toogle.bind(this)}><i className="fa fa-chevron-down"></i> {data.title}</h3>
       <div className="scenario-content">
         <img className="img-responsive" src="/assets/carousel.png" />
         <div className="image-caption">Image Caption</div>
