6da737bb68d037d56f7ab6ae04849370dfbb1d0d
Raphaël Stefanini <raphael.stefanini@anders.fi>
2015-11-11 10:49:40 +0200

    Use "n_votes" instead of "votes"

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -6,7 +6,7 @@ class Comment extends React.Component {
     const {data} = this.props;
     return (<div className="hearing-comment">
       <p>
-        <span className="pull-right"><i className="fa fa-thumbs-o-up"/> {data.votes}</span>
+        <span className="pull-right"><i className="fa fa-thumbs-o-up"/> {data.n_votes}</span>
         <i className="fa fa-clock-o"/> <FormattedRelative value={data.created_at}/><br/>
         <i className="fa fa-user"/> {data.created_by || "-"}
       </p>
