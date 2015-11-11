0f74d236ee0fe949e17611c385ee7df6f3593fed
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-11 10:57:57 +0200

    Wire up Passport and friends for Helsinki auth
    Refs #87

--- server/getSettings.js
+++ server/getSettings.js
@@ -1,36 +1,39 @@
+import {readFileSync} from 'fs';
+import {Provider} from 'nconf';
 const _ = require('lodash');
 
-const defaultSettings = {
-  serverUrl: 'http://127.0.0.1:8086',
+const defaults = {
+  serverUrl: 'http://localhost:8086',
+  publicUrl: null,
+  helsinkiAuthId: null,
+  helsinkiAuthSecret: null,
+  helsinkiTargetApp: 'kerrokantasi',
+  jwtAudience: 'kerrokantasi',
+  jwtKey: null,
+  sessionSecret: null,
   dev: false,
   cold: false
 };
 
-/**
- * Get server options from args and env.
- *
- * @param args Something `minimist` can parse; defaults to the process argv
- * @param env A dictionary of environment variables; defaults to the process env
- */
-export default function getOptions(args = null, env = null) {
-  args = args || require('minimist')(process.argv.slice(2));
-  env = env || process.env;
-  const settings = _.assign({}, defaultSettings);
-  for (var setting in settings) {
-    if (!settings.hasOwnProperty(setting)) {
-      continue;
-    }
-    _.each([
-      setting,
-      _.snakeCase(setting).toUpperCase(),  // "serverUrl" -> "SERVER_URL"
-      _.kebabCase(setting).toLowerCase()  // "serverUrl" -> "server-url"
-    ], function (arg) {
-      if (env[arg]) settings[setting] = env[arg];
-      if (args[arg]) settings[setting] = args[arg];
-    });
-  }
+
+export default function getOptions() {
+  const nconf = new Provider();
+  nconf.argv(); // Args are good
+  nconf.env(); // Envs are good
+  nconf.file({file: './kk.config'}); // Config is good
+  nconf.defaults(_.clone(defaults));  // Defaults are good
+  const settings = _.pick(nconf.get(), Object.keys(defaults));
   const {hostname, port} = require('url').parse(settings.serverUrl);
   settings.hostname = hostname;
   settings.port = port;
+  settings.publicUrl = settings.publicUrl || settings.serverUrl;
+  if (settings.dev) {
+    if(!settings.sessionSecret) {
+      settings.sessionSecret = 'Don\'t Panic.';
+    }
+    if(!settings.jwtKey) {
+      settings.jwtKey = 'kerrokantasi';
+    }
+  }
   return settings;
 }
