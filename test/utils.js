84e5e19d4f848ca9bc5563f93218dbc6f7c891bb
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-11-17 15:33:38 +0200

    Add getRenderPromise test helper

--- test/utils.js
+++ test/utils.js
@@ -1,9 +1,11 @@
 import assign from 'lodash/object/assign';
+import noop from 'lodash/utility/noop';
 import commonInit from 'commonInit';
 import createMemoryHistory from 'history/lib/createMemoryHistory';
 import createStore from 'createStore';
 import messages from 'i18n';
 import React from 'react';
+import express from 'express';
 import {createRenderer} from 'react-addons-test-utils';
 import {IntlProvider} from 'react-intl';
 import {Provider} from 'react-redux';
@@ -34,6 +36,58 @@ export function wireComponent(store, type, props = {}, children = []) {
 export function createTestStore(state) {
   commonInit();
   return createStore(reduxReactRouter, createMemoryHistory, state || {});
+}
+
+/**
+ * Constructor for a mock response object; the `writer`
+ * property is just about enough to pass to our Express renderers.
+ * @param done {Function} Called when the request is finished (when `send` is called).
+ * @constructor
+ */
+function MockResponse(done = noop) {
+  var res = this;
+  var _status = null;
+  var _body = "";
+  const writer = this.writer = {
+    status: (code) => {
+      _status = code;
+      return writer;
+    },
+    json: (json) => {
+      return writer.send(JSON.stringify(json));
+    },
+    send: (data) => {
+      _body += data;
+      done(res);
+    }
+  };
+  res.dump = () => {
+    return {status: _status, body: _body};
+  };
+}
+
+/**
+ * Get a promise for rendering a given request's response.
+ * Server rendering is forced true for these requests.
+ *
+ * @param request Express-like request object. Just setting `url` should be enough.
+ * @param settings Settings object.
+ * @param initialState Initial state.
+ * @return {Promise}
+ */
+export function getRenderPromise(request, initialState = {}, settings = {}) {
+  const render = require('../server/render');
+  settings = assign({}, settings, {serverRendering: true, dev: true});
+  request.__proto__ = express.request;
+  if (!request.method) {
+    request.method = "GET";
+  }
+  return new Promise((resolve, reject) => {
+    const mockResponse = new MockResponse((res) => {
+      resolve(res.dump());
+    });
+    render(request, mockResponse.writer, settings, initialState);
+  });
 }
 
 /**
