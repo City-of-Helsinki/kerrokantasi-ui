364dba21dc47e0f0542a4c8e0fdfa7dc494222f6
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-03-04 12:38:32 +0200

    Add images, labels and comment count to hearing list

--- src/components/HearingList.js
+++ src/components/HearingList.js
@@ -4,20 +4,38 @@ import {injectIntl} from 'react-intl';
 import {Link} from 'react-router';
 import formatRelativeTime from '../utils/formatRelativeTime';
 import Icon from 'utils/Icon';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+import LabelList from './LabelList';
 
 class HearingListItem extends React.Component {
 
   render() {
     const hearing = this.props.hearing;
-    return (<div className="hearing-list-item">
-      <h4 className="hearing-list-item-title">
-        {!hearing.published ? <Icon name="eye-slash" /> : null}
-        <Link to={`/hearing/${hearing.id}`}>{hearing.title}</Link>
-      </h4>
-      <div className="hearing-list-item-meta">
-        <Icon name="clock-o"/> {formatRelativeTime("timeOpen", hearing.open_at)} | {formatRelativeTime("timeClose", hearing.close_at)}
-      </div>
-    </div>);
+    const mainImage = (hearing.images.length ? hearing.images[0] : null);
+    return (<Row className="hearing-list-item">
+      <Col md={4}>
+        {mainImage ? <img src={mainImage.url} className="img-responsive"/> : null}
+      </Col>
+      <Col md={8}>
+        <h4 className="hearing-list-item-title">
+          {!hearing.published ? <Icon name="eye-slash"/> : null}
+          <Link to={`/hearing/${hearing.id}`}>{hearing.title}</Link>
+        </h4>
+        <div className="hearing-list-item-meta hearing-list-item-times">
+          <Icon name="clock-o"/>&nbsp;
+          {formatRelativeTime("timeOpen", hearing.open_at)}
+          &nbsp;|&nbsp;
+          {formatRelativeTime("timeClose", hearing.close_at)}
+        </div>
+        <div className="hearing-list-item-meta hearing-list-item-labels">
+          <LabelList labels={hearing.labels} />
+        </div>
+        <div className="hearing-list-item-meta hearing-list-item-comments">
+          <Icon name="comment-o"/>&nbsp;{ hearing.n_comments }
+        </div>
+      </Col>
+    </Row>);
   }
 }
 
