e6953761bd5ef5357a1f86c9f4679c1c9d83a1ff
Rikuoja <riku.oja@kapsi.fi>
2016-04-06 18:04:40 +0300

    Track all routes in Piwik

--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,25 @@ import Home from 'views/Home';
 import Info from 'views/Info';
 import NotFound from 'views/NotFound';
 
+function trackLink() {
+  /* global Piwik */
+  try {
+    const tracker = Piwik.getTracker("http://analytics.hel.ninja/piwik/piwik.php", 2);
+    tracker.trackLink(document.URL);
+  } catch (err) {
+    /* When entering the site, Piwik is not yet defined. Entering will
+    be tracked by Piwik once it has finished loading.
+     */
+  }
+}
+
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/info" component={Info} />
-    <Route path="/map" component={HearingMap} />
-    <Route path="/hearings" component={AllHearings} />
-    <Route path="/hearing/(:hearingId)" component={Hearing} />
-    <Route path="*" component={NotFound} />
+  <Route path="/" component={App} onEnter={trackLink}>
+    <IndexRoute component={Home} onEnter={trackLink}/>
+    <Route path="/info" component={Info} onEnter={trackLink}/>
+    <Route path="/map" component={HearingMap} onEnter={trackLink}/>
+    <Route path="/hearings" component={AllHearings} onEnter={trackLink}/>
+    <Route path="/hearing/(:hearingId)" component={Hearing} onEnter={trackLink}/>
+    <Route path="*" component={NotFound} onEnter={trackLink}/>
   </Route>
 );
