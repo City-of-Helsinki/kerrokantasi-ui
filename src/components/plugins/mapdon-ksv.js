7cffae05ee94a287ab2b68a53238a22d07c11858
Rikuoja <riku.oja@kapsi.fi>
2016-07-07 17:56:35 +0300

    Inherit plugin from BaseCommentForm to clear user data after posting

--- src/components/plugins/mapdon-ksv.js
+++ src/components/plugins/mapdon-ksv.js
@@ -1,15 +1,18 @@
 import Button from 'react-bootstrap/lib/Button';
-import {MapdonHKRPlugin} from './mapdon-hkr';
+import BaseCommentForm from '../BaseCommentForm';
 import CommentDisclaimer from "../CommentDisclaimer";
 import Input from 'react-bootstrap/lib/Input';
 import React from 'react';
 import {injectIntl} from 'react-intl';
 
 
-class MapdonKSVPlugin extends MapdonHKRPlugin {
+class MapdonKSVPlugin extends BaseCommentForm {
   constructor(props) {
     super(props);
     this.pluginInstanceId = "ksv" + (0 | (Math.random() * 10000000));
+    this.userDataChanged = false;
+    this.lastUserData = null;
+    this.submitting = false;
   }
 
   render() {
@@ -45,16 +48,65 @@ class MapdonKSVPlugin extends MapdonHKRPlugin {
     );
   }
 
+  sendMessageToPluginFrame(message) {
+    this.refs.frame.contentWindow.postMessage(message, "*");
+  }
+
+  requestData() {
+    this.sendMessageToPluginFrame({
+      message: "getUserData",
+      instanceId: this.pluginInstanceId
+    });
+  }
+
+  getDataAndSubmitComment() {
+    this.submitting = true;
+    this.requestData();
+  }
+
+  getPluginData() {
+    return this.lastUserData;
+  }
+
   onReceiveMessage(event) {
     const pluginPurpose = this.props.pluginPurpose;
     // override user messages if in visualization mode
     if (pluginPurpose !== 'postComments') {
       return;
     }
-    super.onReceiveMessage(event);
+    const payload = event.data;
+    if (typeof payload === 'string') {
+      return;
+    }
+    if (payload.instanceId !== this.pluginInstanceId) {
+      return;
+    }
+
+    if (payload.message === "userDataChanged") {
+      this.userDataChanged = true;
+    }
+
+    if (payload.message === "userData") {
+      this.lastUserData = payload.data;
+      if (this.submitting) {
+        this.submitting = false;
+        if (this.lastUserData) {
+          this.submitComment();
+        } else {
+          alert("Et muuttanut mitään kartassa.");
+        }
+      }
+    }
+  }
+
+  clearCommentText() {
+    // after successful posting, user data shall be decimated from the map
+    this.userDataChanged = false;
+    super.clearCommentText();
   }
 
   componentDidMount() {
+    super.componentDidMount();
     const iframe = this.refs.frame;
     const {data, pluginPurpose} = this.props;
     let {comments} = this.props;
@@ -75,6 +127,14 @@ class MapdonKSVPlugin extends MapdonHKRPlugin {
         instanceId: this.pluginInstanceId
       });
     }, false);
+  }
+
+  componentWillUnmount() {
+    super.componentWillUnmount();
+    if (this._messageListener) {
+      window.removeEventListener("message", this._messageListener, false);
+      this._messageListener = null;
+    }
   }
 
   componentWillUpdate() {
