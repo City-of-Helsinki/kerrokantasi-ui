1f51c265497e1cc19d0db05706bf63a1d77ae45e
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2016-10-06 09:15:02 +0300

    Add HearingForm for editing and creating Hearings
    HearingForm is divided into multiple steps that each handle the tasks
    specific to the given step. Sections step has also n amount of
    substeps that are used to manage all the sections related to the
    Hearing.
    
    Idea is that HearingForm can be used to both create new Hearings and
    edit existing Hearings.

---
+++ src/components/admin/HearingFormStep2.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {injectIntl, intlShape, FormattedMessage} from 'react-intl';
+
+import Accordion from 'react-bootstrap/lib/Accordion';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import Panel from 'react-bootstrap/lib/Panel';
+
+import SectionForm from './SectionForm';
+import {addSection, removeSection} from '../../actions/hearingEditor';
+import {getMainSection} from '../../utils/hearing';
+import {hearingShape} from '../../types';
+import {initNewSection} from '../../utils/section';
+
+
+class HearingFormStep2 extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSection: getMainSection(this.props.hearing).id
+    };
+    this.addSection = this.addSection.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.sectionSequence = 0;
+  }
+
+  getDeleteSectionButton(section, sectionID) {
+    if (section.type !== "main") {
+      return (
+        <Button
+          bsStyle="danger"
+          className="pull-right"
+          onClick={() => this.deleteSection(sectionID)}
+        >
+          <FormattedMessage id="deleteSection"/>
+        </Button>
+      );
+    }
+    return null;
+  }
+
+  /*
+  * Get element for each hearing section.
+  * @returns {Array} - Array of Panel elements.
+   */
+  getSections() {
+    const sections = [];
+    for (const section of this.props.hearing.sections) {
+      const sectionHeader = this.props.intl.formatMessage({
+        id: `${section.type}Section`
+      });
+      const sectionID = section.id || section.frontID || "";
+      sections.push(
+        <Panel
+          eventKey={sectionID}
+          header={`${sectionHeader}: ${section.title}`}
+          key={sectionID}
+        >
+          <SectionForm
+            section={section}
+            onSectionChange={this.props.onSectionChange}
+            onSectionImageChange={this.props.onSectionImageChange}
+          />
+          <hr/>
+          {this.getDeleteSectionButton(section, sectionID)}
+        </Panel>
+      );
+    }
+    return sections;
+  }
+
+  handleSelect(activeSection) {
+    this.setState({activeSection});
+  }
+
+  /*
+  * Add new section to the hearing
+  * @param {str} type - Type of the new section to be created
+   */
+  addSection(type) {
+    const newSection = initNewSection();
+    newSection.frontID = (this.sectionSequence += 1).toString();
+    newSection.type = type;
+    this.props.dispatch(addSection(newSection));
+    this.setState({activeSection: newSection.frontID});
+  }
+
+  /*
+  * Remove section with given id.
+  * @param {str} sectionID
+   */
+  deleteSection(sectionID) {
+    this.props.dispatch(removeSection(sectionID));
+  }
+
+  render() {
+    return (
+      <div className="form-step">
+        <Accordion activeKey={this.state.activeSection} onSelect={this.handleSelect}>
+          {this.getSections()}
+        </Accordion>
+        <hr/>
+        <ButtonToolbar className="pull-right">
+          <Button
+            bsStyle="primary"
+            className="pull-right"
+            onClick={this.props.onContinue}
+          >
+            <FormattedMessage id="hearingFormNext"/>
+          </Button>
+          <Button
+            bsStyle="default"
+            className="pull-right"
+            onClick={() => this.addSection("part")}
+          >
+            <FormattedMessage id="addSection"/>
+          </Button>
+          <Button
+            bsStyle="default"
+            className="pull-right"
+            onClick={() => this.addSection("scenario")}
+          >
+            <FormattedMessage id="addOption"/>
+          </Button>
+        </ButtonToolbar>
+      </div>
+    );
+  }
+}
+
+
+HearingFormStep2.propTypes = {
+  dispatch: React.PropTypes.func,
+  hearing: hearingShape,
+  intl: intlShape.isRequired,
+  onContinue: React.PropTypes.func,
+  onHearingChange: React.PropTypes.func,
+  onSectionChange: React.PropTypes.func,
+  onSectionImageChange: React.PropTypes.func,
+};
+
+const WrappedHearingFormStep2 = connect()(injectIntl(HearingFormStep2));
+
+export default WrappedHearingFormStep2;
