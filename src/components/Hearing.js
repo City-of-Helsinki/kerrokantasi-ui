dec9a656d9cf1c4a54e2a5787e106ce0124fb788
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2016-11-30 16:39:55 +0200

    Add edit button to hearing component
    Visible only for those with edit permissions.

--- src/components/Hearing.js
+++ src/components/Hearing.js
@@ -23,10 +23,12 @@ import _, {get, find} from 'lodash';
 import Icon from '../utils/Icon';
 import {
   acceptsComments,
+  canEdit,
   getClosureSection,
+  getHearingEditorURL,
   getHearingURL,
   getMainSection,
-  hasFullscreenMapPlugin
+  hasFullscreenMapPlugin,
 } from '../utils/hearing';
 import {
   isSpecialSectionType,
@@ -45,6 +47,10 @@ export class Hearing extends React.Component {
 
   openFullscreen(hearing) {
     this.props.dispatch(push(getHearingURL(hearing, {fullscreen: true})));
+  }
+
+  toHearingEditor(hearing) {
+    this.props.dispatch(push(getHearingEditorURL(hearing)));
   }
 
   onPostHearingComment(text, authorName, pluginData, geojson, label, images) { // eslint-disable-line
@@ -151,6 +157,18 @@ export class Hearing extends React.Component {
     );
   }
 
+  getManageButton() {
+    const {user, hearing} = this.props;
+    if (canEdit(user, hearing)) {
+      return (
+        <Button bsStyle="primary" onClick={() => this.toHearingEditor(hearing)}>
+          <Icon name="edit"/> <FormattedMessage id="editHearing"/>
+        </Button>
+      );
+    }
+    return null;
+  }
+
   getClosureInfo(hearing) {
     const {formatMessage} = this.props.intl;
     const closureInfo = getClosureSection(hearing);
@@ -248,6 +266,9 @@ export class Hearing extends React.Component {
 
     return (
       <div id="hearing-wrapper">
+        <div className="text-right">
+          {this.getManageButton()}
+        </div>
         <LabelList className="main-labels" labels={hearing.labels}/>
         <Waypoint onEnter={() => changeCurrentlyViewed('#hearing')}/>
         <h1 className="page-title">
