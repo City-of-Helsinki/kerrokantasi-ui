b6611199475e49e393f4e61105ad92ff07e98a2c
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2016-11-30 16:49:08 +0200

    Small fixes and improvements to hearing and section utils

--- src/utils/hearing.js
+++ src/utils/hearing.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import moment from 'moment';
+
 import {initNewSection} from './section';
 
 
@@ -34,6 +36,14 @@ export function getHearingMainImageURL(hearing) {
 
 
 /*
+* Return URL to hearing management view.
+ */
+export function getHearingEditorURL(hearing) {
+  return `/hearing/${hearing.slug}/manage`;
+}
+
+
+/*
 * Returns true if hearing has a plugin that can be rendered fullscreen
 * else false.
  */
@@ -50,6 +60,18 @@ export function acceptsComments(hearing) {
   return !hearing.closed && (new Date() < new Date(hearing.close_at));
 }
 
+
+export function isOpen(hearing) {
+  const openAt = moment(hearing.open_at);
+  const closeAt = moment(hearing.close_at);
+  if (openAt.isValid() && closeAt.isValid()) {
+    const now = moment();
+    if (openAt <= now && closeAt >= now) {
+      return true;
+    }
+  }
+  return false;
+}
 
 /**
  * Get section by given ID or undefined
@@ -86,7 +108,8 @@ export function initNewHearing(inits) {
     sections: [mainSection],
     main_image: {},
     contact_persons: [],
-    n_comments: 0
+    n_comments: 0,
+    isNew: true,
   }, inits || {});
 }
 
