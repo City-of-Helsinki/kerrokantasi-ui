e6b608a0869a874b23912fe018ecf8d0e8e41b23
Rikuoja <riku.oja@kapsi.fi>
2016-12-01 16:17:59 +0200

    Match unknown paths to hearing slugs

--- src/routes.js
+++ src/routes.js
@@ -28,6 +28,6 @@ export default (
     <Route path="/map" component={HearingMap} onEnter={trackLink}/>
     <Route path="/hearings" component={AllHearings} onEnter={trackLink}/>
     <Route path="/hearing/(:hearingSlug)" component={Hearing} onEnter={trackLink}/>
-    <Route path="*" component={NotFound} onEnter={trackLink}/>
+    <Route path="/(:hearingSlug)" component={Hearing} onEnter={trackLink}/>
   </Route>
 );
