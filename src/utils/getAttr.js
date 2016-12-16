5fc7f7ec6738688c9d86d709734556077d51dd82
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-12-16 14:47:39 +0200

    Add options parameter to getAttr and write tests

--- src/utils/getAttr.js
+++ src/utils/getAttr.js
@@ -1,25 +1,41 @@
 import {has, isObject, keys} from 'lodash';
 
+const DefaultOptions = {
+  exact: false,
+  // dev: true
+  dev: false
+};
+
 /**
  * Tries to get translated value from the given attribute. If there's no value
  * for given language, returns any value. If the given attribute was a string
  * instead of object, the function returns the given string.
  * @param  {(object|string)} attr - Translated field retrieved from API
  * @param  {string} lang - Language code for requested language
- * @return {string} Translated value or empty string.
+ * @param  {object} [options]
+ * @return {any} Translated value or unmutated attr if it wasn't object.
+ *               Undefined if the exact translation is not available
+ *               and option `exact === true`.
  */
-const getAttr = (attr, lang) => {
+const getAttr = (attr, lang, options = DefaultOptions) => {
+  const {exact, dev} = options;
   let translated = isObject(attr) && has(attr, lang) ? attr[lang] : attr;
+
+  if (exact && (!isObject(attr) || !translated || isObject(translated))) {
+    return undefined;
+  }
   if (!translated || isObject(translated)) {
     const attrKeys = keys(attr);
     for (let index = 0; index < attrKeys.length; index += 1) {
-      translated = attrKeys[index];
+      translated = attr[attrKeys[index]];
       if (translated) {
         break;
       }
     }
   }
-  // FIXME return translated || '';
-  return translated ? `${translated}-${lang}` : '';
+  if (dev) {
+    return `${translated}-${lang}`;
+  }
+  return translated;
 };
 export default getAttr;
