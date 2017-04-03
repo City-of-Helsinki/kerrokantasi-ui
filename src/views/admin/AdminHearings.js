6d2d74794a4632cd33af00abc6893148c26356f4
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-04-03 14:30:12 +0700

    DRY AdminHearings render method

--- src/views/admin/AdminHearings.js
+++ src/views/admin/AdminHearings.js
@@ -8,21 +8,39 @@ import Button from 'react-bootstrap/lib/Button';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
 import {Tab, Tabs} from 'react-bootstrap';
+import {get} from 'lodash';
 
 import HearingList from '../../components/HearingList';
 import Icon from '../../utils/Icon';
 import {fetchHearingList, fetchLabels} from '../../actions';
 import {labelShape} from '../../types';
 
+const now = () => new Date().toISOString();
+
+const ADMIN_HEARING_LISTS = [
+  {
+    list: 'publishedHearings',
+    params: { published: "True", open_at_lte: now() },
+    formattedMessage: 'publishedHearings',
+  }, {
+    list: 'publishedQueueHearings',
+    params: { published: "True", open_at_gt: now() },
+    formattedMessage: 'publishingQueue',
+  }, {
+    list: 'draftHearings',
+    params: { published: "False" },
+    formattedMessage: 'drafts'
+  }
+];
+
 
 class AdminHearings extends React.Component {
+
   static fetchData(dispatch) {
-    const now = new Date().toISOString();
-    return Promise.all([
-      dispatch(fetchHearingList("publishedHearings", "/v1/hearing/", {published: "True", open_at_lte: now})),
-      dispatch(fetchHearingList("publishingQueueHearings", "/v1/hearing/", {published: "True", open_at_gt: now})),
-      dispatch(fetchHearingList("draftHearings", "/v1/hearing/", {published: "False"}))
-    ]);
+    return Promise.all(
+      ADMIN_HEARING_LISTS.map((list) => dispatch(fetchHearingList(list.list, '/v1/hearing', list.params)))
+    );
+  }
   }
 
   componentDidMount() {
@@ -49,43 +67,26 @@ class AdminHearings extends React.Component {
       </div>
 
       {/* switching tabs doesn't seem to work if animation isn't disabled */}
-      <Tabs defaultActiveKey={1} id="hearing-tabs" animation={false}>
-        <Tab eventKey={1} title={formatMessage({id: 'publishedHearings'})}>
-          <Row>
-            <Col md={8}>
-              <HearingList
-                hearings={hearingLists.publishedHearings.data}
-                isLoading={hearingLists.publishedHearings.isLoading}
-                language={language}
-                labels={labels}
-              />
-            </Col>
-          </Row>
-        </Tab>
-        <Tab eventKey={2} title={formatMessage({id: 'publishingQueue'})}>
-          <Row>
-            <Col md={8}>
-              <HearingList
-                hearings={hearingLists.publishingQueueHearings.data}
-                isLoading={hearingLists.publishingQueueHearings.isLoading}
-                language={language}
-                labels={labels}
-              />
-            </Col>
-          </Row>
-        </Tab>
-        <Tab eventKey={3} title={formatMessage({id: 'drafts'})}>
-          <Row>
-            <Col md={8}>
-              <HearingList
-                hearings={hearingLists.draftHearings.data}
-                isLoading={hearingLists.draftHearings.isLoading}
-                language={language}
-                labels={labels}
-              />
-            </Col>
-          </Row>
-        </Tab>
+      <Tabs defaultActiveKey={0} id="hearing-tabs" animation={false}>
+        {
+          ADMIN_HEARING_LISTS.map((list, index) =>
+            <Tab key={list.list} eventKey={index} title={formatMessage({id: list.formattedMessage})}>
+              <Row>
+                <Col md={8}>
+                  <HearingList
+                    hearings={get(hearingLists, `[${list.list}].data`)}
+                    isLoading={get(hearingLists, `[${list.list}].isLoading`)}
+                    language={language}
+                    labels={labels}
+                    handleChangeFilter={null}
+                    handleSort={null}
+                    handleSearch={null}
+                  />
+                </Col>
+              </Row>
+            </Tab>
+          )
+        }
       </Tabs>
     </div>);
   }
