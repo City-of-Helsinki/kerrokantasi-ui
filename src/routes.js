a1259f44edd9392e52f12b4ed1a20e3bba2d1649
Ville Vaarala <ville.l.vaarala@gmail.com>
2017-05-30 11:16:44 +0700

    Add redirect /hearings -> /hearings/list

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, IndexRoute} from 'react-router';
+import {Route, IndexRoute, Redirect} from 'react-router';
 import App from './App';
 import AllHearings from './views/AllHearings';
 import AdminHearings from './views/admin/AdminHearings';
@@ -32,6 +32,7 @@ export default (
     <Route path="/hearing/new" component={HearingManager} onEnter={trackLink}/>
     <Route path="/info" component={Info} onEnter={trackLink}/>
     <Route path="/map" component={HearingMap} onEnter={trackLink}/>
+    <Redirect from="/hearings" to="hearings/list"/>
     <Route path="/hearings/:tab" component={AllHearings} onEnter={trackLink}/>
     <Route path="/hearing/(:hearingSlug)" component={Hearing} onEnter={trackLink}/>
     <Route path="/hearing/:hearingSlug/:sectionId" component={QuestionView} onEnter={trackLink}/>
