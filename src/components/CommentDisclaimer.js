14b465c0ebdf43199f370f3acb8439683d3c0fc7
Aarni Koskela <akx@iki.fi>
2017-11-23 15:06:32 +0200

    Fix misindented JSX

--- src/components/CommentDisclaimer.js
+++ src/components/CommentDisclaimer.js
@@ -9,12 +9,16 @@ class CommentDisclaimer extends React.Component {
         <FormattedMessage
           id="commentConditions"
           values={{
-            linkToDefinition: <a href="http://www.hri.fi/fi/mita-on-avoin-data/" target="_blank">
-              <FormattedMessage id="asOpenData"/>
-            </a>,
-            linkToLicense: <a href="http://creativecommons.org/licenses/by/4.0/deed.fi" target="_blank">
-              <FormattedMessage id="withOpenLicense"/>
-            </a>
+            linkToDefinition: (
+              <a href="http://www.hri.fi/fi/mita-on-avoin-data/" target="_blank">
+                <FormattedMessage id="asOpenData"/>
+              </a>
+            ),
+            linkToLicense: (
+              <a href="http://creativecommons.org/licenses/by/4.0/deed.fi" target="_blank">
+                <FormattedMessage id="withOpenLicense"/>
+              </a>
+            ),
           }}
         />
       </div>
--- src/components/SectionList.js
+++ src/components/SectionList.js
@@ -10,25 +10,27 @@ class SectionList extends React.Component {
     if (!sections || sections.length === 0) {
       return null;
     }
-    return (<div>
-      <h2>{sections.length === 1 ?
-        sections[0].type_name_singular :
-        sections[0].type_name_plural}
-      </h2>
-      {sections.map((section) => (
-        <WrappedSection
-          section={section}
-          key={section.id}
-          canComment={this.props.canComment && userCanComment(user, section)}
-          onPostComment={this.props.onPostComment}
-          canVote={this.props.canVote && userCanVote(user, section)}
-          onPostVote={this.props.onPostVote}
-          loadSectionComments={this.props.loadSectionComments}
-          comments={this.props.sectionComments[section.id]}
-          user={user}
-          linkTo={`${basePath}/${section.id}`}
-        />))}
-    </div>);
+    return (
+      <div>
+        <h2>{sections.length === 1 ?
+          sections[0].type_name_singular :
+          sections[0].type_name_plural}
+        </h2>
+        {sections.map((section) => (
+          <WrappedSection
+            section={section}
+            key={section.id}
+            canComment={this.props.canComment && userCanComment(user, section)}
+            onPostComment={this.props.onPostComment}
+            canVote={this.props.canVote && userCanVote(user, section)}
+            onPostVote={this.props.onPostVote}
+            loadSectionComments={this.props.loadSectionComments}
+            comments={this.props.sectionComments[section.id]}
+            user={user}
+            linkTo={`${basePath}/${section.id}`}
+          />))}
+      </div>
+    );
   }
 }
 
