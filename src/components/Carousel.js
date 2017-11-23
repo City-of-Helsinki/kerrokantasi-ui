c270561caed5ab1acac1bddc5f1793a1f052b4dc
jonnetanninen <jonne.tanninen@gmail.com>
2017-11-23 17:16:45 +0200

    Add first iteration of carousel and add carousel to question view

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
+import getAttr from '../utils/getAttr';
+import isEmpty from 'lodash/isEmpty';
+import findIndex from 'lodash/findIndex';
+import {getSectionURL} from '../utils/section';
+import {Link, withRouter} from 'react-router-dom';
+import {Row} from 'react-bootstrap';
 
 export class Carousel extends React.Component {
 
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-
-    const {hearing} = this.props;
+    const {hearing, match: params, language} = this.props;
 
     return (
-      <div>
-        <Slider dots {...settings}>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
-          <div><img src='http://placekitten.com/g/400/200' /></div>
+      <div className="carousel-container">
+        <Slider
+          infinite={false}
+          focusOnSelect
+          slidesToShow={4}
+          autoplay={false}
+          afterChange={(index) => { console.log(index) }}
+          initialSlide={params.sectionSlug ? findIndex(hearing.sections, (section) => section.id === params.sectionSlug) : 0}
+          ref={slider => {
+            this.slider = slider;
+          }}
+        >
+          {hearing.sections.map((section) => <div key={section.id}><SliderItem url={getSectionURL(hearing.slug, section)} language={language} section={section} /></div>)}
         </Slider>
-      </div>);
+      </div>
+    );
   }
 }
 
@@ -35,9 +41,16 @@ const SliderItems = ({sections}) => {
   );
 };
 
-const SliderItem = ({section}) => {
+const SliderItem = ({section, url, language}) => {
   return (
-    <div>´WOU SUCH ITEM! {section.title.fi}</div>
+    <div className="sliderItem">
+      <Link to={url}>
+        {!isEmpty(section.images) && <img className="sliderImage" src={section.images[0].url} alt="Section"/>}
+        <div className="sliderItemContent">
+          <div className="sliderItemTitle">{getAttr(section.title, language)}</div>
+        </div>
+      </Link>
+    </div>
   );
 };
 
@@ -53,4 +66,4 @@ SliderItem.propTypes = {
   section: PropTypes.object
 };
 
-export default Carousel;
+export default withRouter(Carousel);
