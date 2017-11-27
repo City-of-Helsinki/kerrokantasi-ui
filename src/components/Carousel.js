4c971ddeae562265756c2ffc0384731926837ba4
jonnetanninen <jonne.tanninen@gmail.com>
2017-11-27 17:10:33 +0200

    Add map and style component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,64 +6,92 @@ import isEmpty from 'lodash/isEmpty';
 import findIndex from 'lodash/findIndex';
 import {getSectionURL} from '../utils/section';
 import {Link, withRouter} from 'react-router-dom';
-import {Row} from 'react-bootstrap';
+import OverviewMap from './OverviewMap';
+import {FormattedMessage} from 'react-intl';
 
-export class Carousel extends React.Component {
+export class SectionCarousel extends React.Component {
+  state = {
+    isMobile: typeof window !== 'undefined' && window.innerWidth < 768
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    this.setState({isMobile: typeof window !== 'undefined' && window.innerWidth < 768});
+  }
 
   render() {
     const {hearing, match: params, language} = this.props;
+    const {isMobile} = this.state;
 
     return (
       <div className="carousel-container">
-        <Slider
-          infinite={false}
-          focusOnSelect
-          slidesToShow={4}
-          autoplay={false}
-          afterChange={(index) => { console.log(index) }}
-          initialSlide={params.sectionSlug ? findIndex(hearing.sections, (section) => section.id === params.sectionSlug) : 0}
-          ref={slider => {
-            this.slider = slider;
-          }}
-        >
-          {hearing.sections.map((section) => <div key={section.id}><SliderItem url={getSectionURL(hearing.slug, section)} language={language} section={section} /></div>)}
-        </Slider>
+        {hearing.geojson && <HearingMap hearing={hearing} />}
+        <div className="slider-container">
+          <Slider
+            className="slider"
+            infinite={false}
+            focusOnSelect
+            slidesToShow={isMobile ? 1 : 4}
+            autoplay={false}
+            afterChange={(index) => { console.log(index) }}
+            initialSlide={params.sectionSlug ? findIndex(hearing.sections, (section) => section.id === params.sectionSlug) : 0}
+            ref={slider => {
+              this.slider = slider;
+            }}
+          >
+            {hearing.sections.map(
+              (section) => <div key={section.id}><SliderItem hearingTitle={hearing.title} url={section.type === 'main' ? `/${hearing.slug}` : getSectionURL(hearing.slug, section)} language={language} section={section} /></div>)}
+          </Slider>
+        </div>
       </div>
     );
   }
 }
 
-const SliderItems = ({sections}) => {
+SectionCarousel.propTypes = {
+  hearing: PropTypes.object
+};
+
+export default withRouter(SectionCarousel);
+
+const HearingMap = ({hearing}) => {
   return (
-    <div>
-      {sections.map((section) => <SliderItem key={Math.random()} section={section} />)}
+    <div className="carousel-map-container">
+      <div className="carousel-map">
+        <OverviewMap hearings={[hearing]} style={{width: '100%', height: '200px'}} hideIfEmpty />
+      </div>
     </div>
   );
 };
 
-const SliderItem = ({section, url, language}) => {
+HearingMap.propTypes = {
+  hearing: PropTypes.object
+};
+
+const SliderItem = ({section, url, language, hearingTitle}) => {
   return (
-    <div className="sliderItem">
+    <div className="slider-item">
       <Link to={url}>
-        {!isEmpty(section.images) && <img className="sliderImage" src={section.images[0].url} alt="Section"/>}
-        <div className="sliderItemContent">
-          <div className="sliderItemTitle">{getAttr(section.title, language)}</div>
+        {!isEmpty(section.images) && <img className="slider-image" src={section.images[0].url} alt="Section"/>}
+        <div className="slider-item-content">
+          <div className="slider-item-title">{section.type === 'main' ? getAttr(hearingTitle, language) : getAttr(section.title, language)}</div>
         </div>
       </Link>
     </div>
   );
 };
 
-Carousel.propTypes = {
-  hearing: PropTypes.object
-};
-
-SliderItems.propTypes = {
-  sections: PropTypes.array
-};
 
 SliderItem.propTypes = {
-  section: PropTypes.object
+  section: PropTypes.object,
+  url: PropTypes.string,
+  language: PropTypes.string,
+  hearingTitle: PropTypes.object
 };
-
-export default withRouter(Carousel);
