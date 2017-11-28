62096e6b20199e1d9631179c4fe8271fdd71e3b7
jonnetanninen <jonne.tanninen@gmail.com>
2017-11-28 10:05:25 +0200

    Fix tests so they work with react-slick

--- test-setup.js
+++ test-setup.js
@@ -1,4 +1,14 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
+// Basic configuration of Jest + Enzyme tests on React v15
 configure({adapter: new Adapter()});
+
+// Needed for tests to work with react-slick, check https://github.com/akiran/react-slick#test-setup
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: () => {},
+    removeListener: () => {}
+  };
+};
