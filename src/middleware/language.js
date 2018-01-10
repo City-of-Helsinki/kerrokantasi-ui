763e1fd06922cc57603ef1dc39e51e604479d7bf
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-10 10:31:13 +0200

    Add support if lang does not exist or if lang is not supported

--- src/middleware/language.js
+++ src/middleware/language.js
@@ -1,10 +1,22 @@
 import {setLanguage} from '../actions';
-import {parseQuery} from '../utils/urlQuery';
+import {stringifyQuery, parseQuery} from '../utils/urlQuery';
+import isEmpty from 'lodash/isEmpty';
+import find from 'lodash/find';
+import {push} from 'react-router-redux';
+import config from '../config';
 
 export const languageFromUrlMiddleware = store => next => action => {
-  if (action.type !== '@@router/LOCATION_CHANGE') return next(action);
-
-  store.dispatch(setLanguage(parseQuery(action.payload.search).lang));
+  if (action.type !== '@@router/LOCATION_CHANGE') {
+    return next(action);
+  }
+  if (isEmpty(parseQuery(action.payload.search).lang) || isEmpty(find(config.languages, (lang) => lang === parseQuery(action.payload.search).lang))) {
+    store.dispatch(push({location: action.payload.pathname, search: stringifyQuery({lang: store.getState().language})}));
+    return next(action);
+  }
+  if (parseQuery(action.payload.search).lang !== store.getState().language) {
+    store.dispatch(setLanguage(parseQuery(action.payload.search).lang));
+    return next(action);
+  }
   return next(action);
 };
 
