890dfb31727474190d11bbfa3efada52a452fe7f
Juhana Leinonen <juhana.leinonen@digia.com>
2018-01-12 10:36:00 +0200

    Fix warnings about bad DOM in SortableCommentList

--- src/components/SortableCommentList.js
+++ src/components/SortableCommentList.js
@@ -195,15 +195,12 @@ export class SortableCommentList extends Component {
                           onChange={event => {
                             this.fetchComments(section.id, event.target.value);
                           }}
+                          value={get(sectionComments, 'ordering')}
                         >
                           {keys(ORDERING_CRITERIA).map(key =>
-                            <option
-                              key={key}
-                              value={ORDERING_CRITERIA[key]}
-                              selected={ORDERING_CRITERIA[key] === get(sectionComments, 'ordering')}
-                            >
-                              <FormattedMessage id={key} />
-                            </option>,
+                              <FormattedMessage id={key} key={key}>
+                                {(message) => <option value={ORDERING_CRITERIA[key]}>{message}</option>}
+                              </FormattedMessage>
                           )}
                         </FormControl>
                       </div>
