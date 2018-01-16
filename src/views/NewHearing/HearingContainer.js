4b34531ba71a5e73902b5fae81a441f97dab4c50
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-16 16:12:46 +0200

    Add HearingEditor

--- src/views/NewHearing/HearingContainer.js
+++ src/views/NewHearing/HearingContainer.js
@@ -11,27 +11,56 @@ import LoadSpinner from '../../components/LoadSpinner';
 import isEmpty from 'lodash/isEmpty';
 import { injectIntl, intlShape } from 'react-intl';
 import {SectionTypes} from '../../utils/section';
+import {canEdit} from '../../utils/hearing';
+import HearingEditor from '../../components/admin/HearingEditor';
+import * as HearingEditorSelector from '../../selectors/hearingEditor';
+import { initNewHearing, fetchHearingEditorMetaData } from '../../actions/hearingEditor';
 
 export class HearingContainer extends React.Component {
   componentWillMount() {
-    const {fetchHearing, match: {params}} = this.props;
+    const {fetchHearing, fetchEditorMetaData, match: {params}} = this.props;
     fetchHearing(params.hearingSlug);
+    fetchEditorMetaData();
   }
 
   render() {
-    const {hearing, intl, language, match} = this.props;
+    const {
+      hearing,
+      intl,
+      language,
+      match,
+      user,
+      labels,
+      hearingDraft,
+      hearingLanguages,
+      isLoading,
+      contactPersons
+    } = this.props;
     const mainSectionId = !isEmpty(hearing) ? hearing.sections.find(section => section.type === SectionTypes.MAIN).id : null;
 
     return (
       <div className="hearing-page">
+
         {!isEmpty(hearing) ?
-          <div className="hearing-wrapper" id="hearing-wrapper">
-            <Header hearing={hearing} activeLanguage={language} intl={intl}/>
-            <WrappedCarousel hearing={hearing} intl={intl} language={language}/>
-            <Switch>
-              <Route path="/:hearingSlug/:sectionId" component={Section} />
-              <Redirect from="/:hearingSlug" to={`/${match.params.hearingSlug}/${mainSectionId}`} />
-            </Switch>
+          <div>
+            {(!isEmpty(user) && canEdit(user, hearing)) &&
+              <HearingEditor
+                hearing={hearingDraft}
+                hearingLanguages={hearingLanguages}
+                labels={labels}
+                user={user}
+                isLoading={isLoading}
+                contactPersons={contactPersons}
+              />
+            }
+            <div className="hearing-wrapper" id="hearing-wrapper">
+              <Header hearing={hearing} activeLanguage={language} intl={intl}/>
+              <WrappedCarousel hearing={hearing} intl={intl} language={language}/>
+              <Switch>
+                <Route path="/:hearingSlug/:sectionId" component={Section} />
+                <Redirect from="/:hearingSlug" to={`/${match.params.hearingSlug}/${mainSectionId}`} />
+              </Switch>
+            </div>
           </div>
         : <LoadSpinner />
       }
@@ -50,11 +79,18 @@ HearingContainer.propTypes = {
 
 const mapStateToProps = (state, ownProps) => ({
   hearing: getHearingWithSlug(state, ownProps.match.params.hearingSlug),
-  language: state.language
+  language: state.language,
+  hearingDraft: HearingEditorSelector.getPopulatedHearing(state),
+  hearingLanguages: state.hearingEditor.languages,
+  labels: HearingEditorSelector.getLabels(state),
+  user: state.user,
+  isLoading: HearingEditorSelector.getIsLoading(state),
+  contactPersons: HearingEditorSelector.getContactPersons(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchHearing: (hearingSlug, preview = false) => dispatch(fetchHearingAction(hearingSlug, preview))
+  fetchHearing: (hearingSlug, preview = false) => dispatch(fetchHearingAction(hearingSlug, preview)),
+  fetchEditorMetaData: () => dispatch(fetchHearingEditorMetaData())
 });
 
 export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(HearingContainer));
