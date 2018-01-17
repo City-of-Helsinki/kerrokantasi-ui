89f5a0f3b8c622dccbc1d6a7af06e9cf7d4125d1
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-17 13:29:13 +0200

    Update tests

--- __tests__/Hearing/SectionContainer.react-test.js
+++ __tests__/Hearing/SectionContainer.react-test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
-import {SectionContainer} from '../../src/components/Hearing/Section/SectionContainer';
+import {SectionContainerComponent} from '../../src/components/Hearing/Section/SectionContainer';
 import {MemoryRouter} from 'react-router-dom';
 import {mockStore, getIntlAsProp} from '../../test-utils';
 
 // Renders the Hearings component using enzymes shallow rendering.
 // You can pass props you want to override as a parameter.
 const setup = propOverrides => {
-  const {labels, sectionComments, hearingLists: {allHearings}, hearing, dispatch} = mockStore;
+  const {labels, sectionComments, hearingLists: {allHearings}, mockHearingWithSections, user} = mockStore;
   const props = Object.assign({
     labels: labels.data,
-    hearing,
+    hearing: mockHearingWithSections.data,
     hearingDraft: {},
     match: {
       params: {
@@ -22,10 +22,14 @@ const setup = propOverrides => {
       pathname: '/' + allHearings.data[0].slug
     },
     sectionComments,
-    dispatch
+    showClosureInfo: true,
+    sections: mockHearingWithSections.data.sections,
+    language: 'fi',
+    contacts: mockHearingWithSections.data.contacts,
+    user
   }, propOverrides);
 
-  const wrapper = shallow(<MemoryRouter><SectionContainer intl={getIntlAsProp()} {...props} /></MemoryRouter>);
+  const wrapper = shallow(<MemoryRouter><SectionContainerComponent intl={getIntlAsProp()} {...props} /></MemoryRouter>);
 
   return {
     props,
