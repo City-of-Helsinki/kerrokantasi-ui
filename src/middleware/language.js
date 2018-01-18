79377b0500abb1c603a7f987cd1baed94919821c
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-18 14:21:01 +0200

    Add hack to make the browser back button to work again

--- src/middleware/language.js
+++ src/middleware/language.js
@@ -10,10 +10,18 @@ export const languageFromUrlMiddleware = store => next => action => {
   if (action.type !== '@@router/LOCATION_CHANGE') {
     return next(action);
   }
-  if (isEmpty(parseQuery(action.payload.search).lang) || isEmpty(find(config.languages, (lang) => lang === parseQuery(action.payload.search).lang))) {
-    store.dispatch(push({location: action.payload.pathname, search: stringifyQuery(merge(parseQuery(action.payload.search), {lang: store.getState().language}))}));
+  // This will allow browser back button to work, maybe not optimal but seems to do the job...
+  if (store.getState().router.location && action.payload.pathname === store.getState().router.location.pathname && isEmpty(parseQuery(action.payload.search).lang)) {
+    window.history.go(-2);
     return next(action);
   }
+  // This will add the language query to url if missing or replace invalid language with the current one
+  if (isEmpty(parseQuery(action.payload.search).lang) || isEmpty(find(config.languages, (lang) => lang === parseQuery(action.payload.search).lang))) {
+    store.dispatch(push({location: action.payload.pathname, search: stringifyQuery(merge(parseQuery(action.payload.search), {lang: store.getState().language}))}));
+    if (store.getState().routerHistory.current !== action.payload.pathname) store.dispatch(updateHistory(action.payload.pathname));
+    return next(action);
+  }
+  // This will change the current language to store if new is legit and different than current one
   if (parseQuery(action.payload.search).lang !== store.getState().language) {
     store.dispatch(setLanguage(parseQuery(action.payload.search).lang));
     return next(action);
