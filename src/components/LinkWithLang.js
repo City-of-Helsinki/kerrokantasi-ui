d7427d8d2473c95433ba52906b3562d6083ea1b5
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-29 14:33:23 +0200

    Add LinkWithLang component

---
+++ src/components/LinkWithLang.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+
+class LinkWithLangComponent extends React.Component {
+  render() {
+    const {to, className, children, language} = this.props;
+    const newTo = {
+      pathname: to.path,
+      search: `${to.search ? to.search + `&lang=${language}` : `?lang=${language}`}`,
+      hash: to.hash || '',
+      state: to.state || {}
+    };
+
+    return (
+      <Link className={className} to={newTo}>{children}</Link>
+    );
+  }
+}
+
+LinkWithLangComponent.propTypes = {
+  to: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    search: PropTypes.string
+  }).isRequired,
+  children: PropTypes.any,
+  className: PropTypes.string,
+  language: PropTypes.string
+};
+
+const mapStateToProps = (state) => ({
+  language: state.language
+});
+
+export default withRouter(connect(mapStateToProps)(LinkWithLangComponent));
