8a5c61d670ff9ea779a89bcf2af93521acf3ad73
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-29 15:49:13 +0200

    Import correct Link

--- src/components/FullWidthHearing.js
+++ src/components/FullWidthHearing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import Link from './LinkWithLang';
 import Icon from '../utils/Icon';
 import FormatRelativeTime from '../utils/FormatRelativeTime';
 import {getHearingURL, getHearingMainImageURL} from '../utils/hearing';
