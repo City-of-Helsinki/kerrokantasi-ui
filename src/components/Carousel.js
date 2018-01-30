b63a8a50bf3f58690f6b115889ecaf0401134ec8
jonnetanninen <jonne.tanninen@gmail.com>
2018-01-30 15:39:50 +0200

    Refocus slider on route change

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -16,91 +16,108 @@ import {FormattedMessage, intlShape} from 'react-intl';
 import Icon from '../utils/Icon';
 import {Grid} from 'react-bootstrap';
 
-export const SectionCarousel = ({hearing, match: {params}, language}) => {
-  const sectionsWithoutClosure = hearing.sections.filter((section) => section.type !== 'closure-info');
+export class SectionCarousel extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    const {hearing, match: {params}} = this.props;
+    if (params.sectionId !== nextProps.match.params.sectionId) {
+      this.slider.slickGoTo(getInitialSlideIndex(hearing, nextProps.match.params));
+    }
+  }
+
   /* Add slide per every section and map as first item if the hearing has geojson and fullscreen link as last item if
    * the hearing in question has fullscreen plugin
    */
-  const slides = sectionsWithoutClosure.map(
-    (section) =>
-      <div key={section.id}>
-        <SliderItem
-          active={(isMainSection(section) && !params.sectionId) || (params.sectionId && section.id === params.sectionId.split('#')[0])}
-          hearingTitle={hearing.title}
-          url={isMainSection(section) ? getHearingURL(hearing) : getSectionURL(hearing.slug, section) + '#start'}
-          language={language}
-          section={section}
-        />
-      </div>);
-  if (hearing.geojson) {
-    slides.unshift(
-      <div key="map">
-        <div className="slider-item">
-          <HearingMap hearing={hearing} />
-        </div>
-      </div>
-    );
-  }
-  if (hasFullscreenMapPlugin(hearing)) {
-    slides.push(
-      <div key="fullscreen">
-        <div className="slider-item">
-          <Link to={getHearingURL(hearing, {fullscreen: true})}>
-            <div
-              className="slider-item-content carousel-fullscreenbutton"
-              style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '191px'}}
-            >
-              <div className="slider-item-title">
-                <FormattedMessage id="openFullscreenMap" />
-                <div style={{marginTop: '15px'}}>
-                  <Icon name="arrows-alt" fixedWidth />
-                </div>
-              </div>
-            </div>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+  getSlides = () => {
+    const {hearing, match: {params}, language} = this.props;
+    const sectionsWithoutClosure = hearing.sections.filter((section) => section.type !== 'closure-info');
 
-  return (
-    <div className="subnav-section">
-      <Grid>
-        <div className="carousel-container">
-          <div id="start" />
-          <div className="slider-container">
-            <Slider
-              className="slider"
-              ref={slider => {
-                this.slider = slider;
-              }}
-              initialSlide={getInitialSlideIndex(hearing, params)}
-              infinite={false}
-              focusOnSelect
-              autoplay={false}
-              centerMode
-              centerPadding="50px"
-              responsive={[{
-                breakpoint: 768,
-                settings: { slidesToShow: 1 }
-              },
-              {
-                breakpoint: 1200,
-                settings: { slidesToShow: 3 }
-              },
-              {
-                breakpoint: 100000,
-                settings: { slidesToShow: 5 }
-              }]}
-            >
-              {slides}
-            </Slider>
+    const slides = sectionsWithoutClosure.map(
+      (section) =>
+        <div key={section.id}>
+          <SliderItem
+            active={(isMainSection(section) && !params.sectionId) || (params.sectionId && section.id === params.sectionId.split('#')[0])}
+            hearingTitle={hearing.title}
+            url={isMainSection(section) ? getHearingURL(hearing) : getSectionURL(hearing.slug, section) + '#start'}
+            language={language}
+            section={section}
+          />
+        </div>);
+    if (hearing.geojson) {
+      slides.unshift(
+        <div key="map">
+          <div className="slider-item">
+            <HearingMap hearing={hearing} />
           </div>
         </div>
-      </Grid>
-    </div>
-  );
-};
+      );
+    }
+    if (hasFullscreenMapPlugin(hearing)) {
+      slides.push(
+        <div key="fullscreen">
+          <div className="slider-item">
+            <Link to={getHearingURL(hearing, {fullscreen: true})}>
+              <div
+                className="slider-item-content carousel-fullscreenbutton"
+                style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '191px'}}
+              >
+                <div className="slider-item-title">
+                  <FormattedMessage id="openFullscreenMap" />
+                  <div style={{marginTop: '15px'}}>
+                    <Icon name="arrows-alt" fixedWidth />
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+    return slides;
+  }
+
+  render() {
+    const {hearing, match: {params}} = this.props;
+
+
+    return (
+      <div className="subnav-section">
+        <Grid>
+          <div className="carousel-container">
+            <div id="start" />
+            <div className="slider-container">
+              <Slider
+                className="slider"
+                ref={slider => {
+                  this.slider = slider;
+                }}
+                initialSlide={getInitialSlideIndex(hearing, params)}
+                infinite={false}
+                focusOnSelect
+                autoplay={false}
+                centerMode
+                centerPadding="50px"
+                responsive={[{
+                  breakpoint: 768,
+                  settings: { slidesToShow: 1 }
+                },
+                {
+                  breakpoint: 1200,
+                  settings: { slidesToShow: 3 }
+                },
+                {
+                  breakpoint: 100000,
+                  settings: { slidesToShow: 5 }
+                }]}
+              >
+                {this.getSlides()}
+              </Slider>
+            </div>
+          </div>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 SectionCarousel.propTypes = {
   hearing: PropTypes.object,
