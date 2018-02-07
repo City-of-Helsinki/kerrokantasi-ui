22cff77382e0959b4213058ffebc5b0e0198a34f
jonnetanninen <jonne.tanninen@gmail.com>
2018-02-07 17:21:21 +0200

    Fix error caused by rebase

--- src/views/Hearing/HearingContainer.js
+++ src/views/Hearing/HearingContainer.js
@@ -105,11 +105,7 @@ HearingContainerComponent.propTypes = {
   fetchEditorMetaData: PropTypes.func,
   setLanguage: PropTypes.func,
   history: PropTypes.object,
-<<<<<<< HEAD
   location: PropTypes.object
-=======
-  location: PropTypes
->>>>>>> Use LinkWithLang instead of Link and force use of to as object instead of string
 };
 
 const mapStateToProps = (state, ownProps) => ({
