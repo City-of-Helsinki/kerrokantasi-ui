013b20bca6fa1e20f9b5c007d9d8eaa2f240774c
jonnetanninen <jonne.tanninen@gmail.com>
2018-02-12 14:08:06 +0200

    Update snapshots

--- src/components/SocialBar/Twitter.js
+++ src/components/SocialBar/Twitter.js
@@ -3,6 +3,6 @@ import {Share} from 'react-twitter-widgets';
 
 export default class Twitter extends React.Component {
   render() {
-    return <div style={{marginRight: '8px'}}><Share className="twitter-tweet-ctr" url={window.location.href} /></div>; // <span ref="container" className="twitter-tweet-ctr" id={"twitter-" + this.state.id}/>;
+    return <div className="twitter-tweet-ctr" style={{marginRight: '8px'}}><Share url={window.location.href} /></div>; // <span ref="container" className="twitter-tweet-ctr" id={"twitter-" + this.state.id}/>;
   }
 }
