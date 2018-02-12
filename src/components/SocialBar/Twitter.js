04f569989fa23943a3330ecba0ff6c3b44d61fcc
jonnetanninen <jonne.tanninen@gmail.com>
2018-02-12 13:22:05 +0200

    Fix social buttons so they update the shared url when changing subsection

--- src/components/SocialBar/Twitter.js
+++ src/components/SocialBar/Twitter.js
@@ -1,21 +1,8 @@
 import React from "react";
-import {loadScriptThenCall} from "./utils";
+import {Share} from 'react-twitter-widgets';
 
 export default class Twitter extends React.Component {
-  componentWillMount() {
-    this.setState({id: Math.random().toString(36)});
-  }
-
-  componentDidMount() {
-    const setup = this.setupTwitterWidget.bind(this);
-    loadScriptThenCall("twitter-wjs", "//platform.twitter.com/widgets.js", "twttr", setup);
-  }
-
-  setupTwitterWidget(twttr) {
-    twttr.widgets.createShareButton(typeof window !== 'undefined' ? window.location.href : '', this.refs.container, {});
-  }
-
   render() {
-    return <span ref="container" className="twitter-tweet-ctr" id={"twitter-" + this.state.id}/>;
+    return <div style={{marginRight: '8px'}}><Share className="twitter-tweet-ctr" url={window.location.href} /></div>; // <span ref="container" className="twitter-tweet-ctr" id={"twitter-" + this.state.id}/>;
   }
 }
