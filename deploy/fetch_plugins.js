b0c1ce6c0e915f45ea320c4a10cef82bdb8cf48a
Ville Koivunen <ville.koivunen@keskiespoo.net>
2018-03-12 15:54:51 +0200

    Move to using shared config for deployer & UI for plugin locations

--- deploy/fetch_plugins.js
+++ deploy/fetch_plugins.js
@@ -2,28 +2,30 @@ const git = require('simple-git')();
 const path = require('path');
 const fs = require('fs');
 
-var base_checkout_path = 'assets/plugins';
-
 if (process.argv.length > 2) {
     base_checkout_path = process.argv[2]
 }
 
-repositories = [{repo:'https://github.com/jvanamo/kerrokantasi-citybikes', directory:'kerrokantasi-citybikes'},
-                {repo:'https://github.com/jvanamo/kerrokantasi-budgeting', directory:'kerrokantasi-budgeting'},
-                {repo:'https://github.com/jvanamo/kerrokantasi-bikeracks', directory:'kerrokantasi-bikeracks'},
-                {repo:'https://github.com/City-of-Helsinki/kerrokantasi-winterbiking', directory:'kerrokantasi-winterbiking'}];
+const repositories = JSON.parse(fs.readFileSync('src/shared_config.json'))['pluginMap'];
 
-for (repo of repositories) {
-    var checkout_path = path.posix.join(base_checkout_path, repo.directory);
-    clone_repo(repo.repo, checkout_path);
+console.log("Fetching plugins...")L
+
+for (repo in repositories) {
+    var checkout_path = repositories[repo].path;
+    clone_repo(repositories[repo].repo, checkout_path);
 }
 
-function clone_repo(repo, path) {
-    if (fs.existsSync(checkout_path)) {
-        console.log(`${path} for ${repo} already exists, skipping`)
+function clone_repo(repo, checkout_path) {
+    // UI uses absolute paths, as they contain the URI root
+    // we want to relativize them for git clone
+    if (path.isAbsolute(checkout_path)) {
+        checkout_path = checkout_path.substr(1);
+    }
+    if (fs.existsSync(path.join(checkout_path, ".git"))) {
+        console.log(`${checkout_path} for ${repo} already exists, skipping`);
     } else {
-        console.log(`Queuing clone of ${repo} to ${path}`);
-        git.clone(repo, path, function(err, output) {
+        console.log(`Queuing clone of ${repo} to ${checkout_path}`);
+        git.clone(repo, checkout_path, function(err, output) {
             // On success error will be null, output seems to be run on quiet
             if (err) {
                 console.log(`${repo} cloning failed`);
