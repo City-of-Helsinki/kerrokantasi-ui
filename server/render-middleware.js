2a55ef7b962fb3ca83d2585d4cd7d54acddba9c9
jonnetanninen <jonne.tanninen@gmail.com>
2018-03-15 15:31:03 +0200

    Add Open Graph tags to html

--- server/render-middleware.js
+++ server/render-middleware.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import {renderToStaticMarkup} from 'react-dom/server';
 import Html from './Html';
+import fetch from 'node-fetch';
 
+const hearingDataCache = {};
+function getHearingDataCached(settings, hearingSlug, ttl = 30 * 60 * 1000) {
+  const now = +new Date(); // current time as milliseconds
+  if (
+    hearingDataCache[hearingSlug] && // in cache
+    now < hearingDataCache[hearingSlug].ts + ttl // not expired yet
+  ) {
+    return hearingDataCache[hearingSlug].data;
+  }
+  return fetch(
+    `${settings.kerrokantasi_api_base}/v1/hearing/${hearingSlug}`
+  )
+    .then(res => (res.status === 404 ? null : res.json())) // Not found? Okay, never mind, must've been a false positive
+    .catch(err => { console.log(err); }) // Other error? Meh, render w/o metatags then
+    .then(data => {
+      // Save in cache while we're here. (Note that 404s and errors are also cached.)
+      hearingDataCache[hearingSlug] = { data, ts: now };
+      return data;
+    });
+}
 
 function renderHTMLSkeleton(req, res, settings) {
-  const html = renderToStaticMarkup(
-    <Html
-      bundleSrc={settings.bundleSrc || '/app.js'}
-      apiBaseUrl={settings.kerrokantasi_api_base}
-      uiConfig={settings.uiConfig}
-    />
-  );
-  res.status(200).send(html);
+  const hearingSlugMatch = req.path.split('/');
+  const hearingDataPromise = hearingSlugMatch
+    ? getHearingDataCached(settings, hearingSlugMatch[1])
+    : Promise.resolve(null);
+  return hearingDataPromise.then(hearingData => {
+    const html = renderToStaticMarkup(
+      <Html
+        bundleSrc={settings.bundleSrc || '/app.js'}
+        apiBaseUrl={settings.kerrokantasi_api_base}
+        uiConfig={settings.uiConfig}
+        hearingData={hearingData}
+      />
+    );
+    res.status(200).send(html);
+  });
 }
 
 export default function renderMiddleware(settings) {
