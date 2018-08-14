c09a75eb08a1d35c93c4f5800e7bdc0c8d0d2dfe
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-08-14 14:53:41 +0300

    Only fetch all comments when visualizer is on

--- src/components/SortableCommentList.js
+++ src/components/SortableCommentList.js
@@ -52,16 +52,14 @@ export class SortableCommentListComponent extends Component {
 
   fetchComments(sectionId, ordering) {
     // if a plugin is involved, we must fetch all the comments for display, not just a select few
-    const {fetchComments, fetchAllComments, section, hearingSlug} = this.props;
-    if (section.plugin_identifier) {
+    const {fetchComments, fetchAllComments, section, hearingSlug, displayVisualization} = this.props;
+    if (displayVisualization && section.plugin_identifier) {
       return fetchAllComments(hearingSlug, sectionId, ordering);
     }
     return fetchComments(sectionId, ordering);
   }
 
   componentDidMount() {
-    // Deprecated for now since it's call site does not pass hearingSlug as it should
-    return;
     const {section, sectionComments} = this.props;
     // comment fetching may already be taking place in the plugin!
     if (!get(sectionComments, 'isFetching')) {
