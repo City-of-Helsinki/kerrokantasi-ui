cd542900f064cc7dd3cfc08985827bd568f863c0
Ville Koivunen <ville.koivunen@hel.fi>
2019-03-26 19:28:08 +0200

    fetch_plugins: add support for specifying where plugins as fetched

--- deploy/fetch_plugins.js
+++ deploy/fetch_plugins.js
@@ -2,8 +2,14 @@ const git = require('simple-git')();
 const path = require('path');
 const fs = require('fs');
 
+// base_checkout_path overrides the leading components of the
+// plugins paths specified in shared_config.json
+// This is for installing on server, where the HTTP server
+// is configured to provide /assets/plugins
 if (process.argv.length > 2) {
-    base_checkout_path = process.argv[2]
+    base_checkout_path = process.argv[2];
+} else {
+    base_checkout_path = "";
 }
 
 const repositories = JSON.parse(fs.readFileSync('src/shared_config.json'))['pluginMap'];
@@ -20,6 +26,12 @@ function clone_repo(repo, checkout_path) {
     // we want to relativize them for git clone
     if (path.isAbsolute(checkout_path)) {
         checkout_path = checkout_path.substr(1);
+    }
+    // However, for base_checkout_path we will just overwrite
+    // all but the actual plugin name
+    if (base_checkout_path) {
+        checkout_name = path.parse(checkout_path).name;
+        checkout_path = path.join(base_checkout_path, checkout_name);
     }
     if (fs.existsSync(path.join(checkout_path, ".git"))) {
         console.log(`${checkout_path} for ${repo} already exists, skipping`);
