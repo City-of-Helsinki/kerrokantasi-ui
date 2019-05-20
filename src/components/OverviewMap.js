9368601307abb1536aa182d3eb58768ba1ae4d76
Anwar Farrukh <faanwar@IT-L-M00194.local>
2019-05-20 13:42:55 +0300

    resize listeners and default width and height for overview map container

--- src/components/OverviewMap.js
+++ src/components/OverviewMap.js
@@ -4,13 +4,48 @@ import PropTypes from 'prop-types';
 import {getHearingURL} from '../utils/hearing';
 import getAttr from '../utils/getAttr';
 import {EPSG3067} from '../utils/map';
-import Leaflet from 'leaflet';
+import Leaflet, { LatLng } from 'leaflet';
+import { Polygon, Marker, Polyline, Map, TileLayer, FeatureGroup, Popup, GeoJSON } from 'react-leaflet'; 
+import 'leaflet/dist/leaflet.css'
 
 class OverviewMap extends React.Component {
+
+  state = {
+    height: null,
+    width: null,
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.updateDimensions)
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.mapContainer && nextProps.mapContainer.getBoundingClientRect()) {
+      this.handleUpdateMapDimensions(nextProps.mapContainer);
+    }
+  }
+
+  updateDimensions = () => {
+    this.handleUpdateMapDimensions(this.props.mapContainer);
+  }
+
+  /**
+   * The react-leaflet requires static width and height to display properly, attach listener.
+   * @param {Object} mapContainer - Container enclosing the map
+   */
+  handleUpdateMapDimensions = (mapContainer) => {
+    const { width, height } = mapContainer.getBoundingClientRect();
+    this.setState({ width: `${width}px`, height: `${height}px`});
+  }
+
   getHearingMapContent(hearings) {
-    const {Popup, GeoJSON} = require('react-leaflet');  // Late import to be isomorphic compatible
     const {language} = this.context;
     const contents = [];
+
     hearings.forEach((hearing) => {
       const {geojson, id} = hearing;
       const content = (
@@ -25,16 +60,15 @@ class OverviewMap extends React.Component {
           </Popup>
         ) : null
       );
+
       if (geojson) {
-        const {LatLng} = require('leaflet');  // Late import to be isomorphic compatible
-        const {Polygon, Marker, Polyline} = require('react-leaflet');  // Late import to be isomorphic compatible
         switch (geojson.type) {
           case "Polygon": {
             // XXX: This only supports the _first_ ring of coordinates in a Polygon
             const latLngs = geojson.coordinates[0].map(([lng, lat]) => new LatLng(lat, lng));
             contents.push(<Polygon key={Math.random()} positions={latLngs}>{content}</Polygon>);
           }
-            break;
+          break;
           case "Point": {
             const latLngs = new LatLng(geojson.coordinates[0], geojson.coordinates[1]);
             contents.push(
@@ -51,15 +85,15 @@ class OverviewMap extends React.Component {
               />
             );
           }
-            break;
+          break;
           case "LineString": {
             const latLngs = geojson.coordinates.map(([lng, lat]) => new LatLng(lat, lng));
             contents.push(<Polyline key={Math.random()} positions={latLngs}>{content}</Polyline>);
           }
-            break;
+          break;
           default:
-            // TODO: Implement support for other geometries too (markers, square, circle)
-            contents.push(<GeoJSON data={geojson} key={JSON.stringify(geojson)}>{content}</GeoJSON>);
+          // TODO: Implement support for other geometries too (markers, square, circle)
+          contents.push(<GeoJSON data={geojson} key={JSON.stringify(geojson)}>{content}</GeoJSON>);
         }
         contents.push(<GeoJSON key={id} data={geojson}>{content}</GeoJSON>);
       }
@@ -70,7 +104,6 @@ class OverviewMap extends React.Component {
   render() {
     if (typeof window === "undefined") return null;
     const {style, hearings} = this.props;
-    const {Map, TileLayer, FeatureGroup} = require('react-leaflet');  // Late import to be isomorphic compatible
     const contents = this.getHearingMapContent(hearings);
     if (!contents.length && this.props.hideIfEmpty) {
       return null;
@@ -78,7 +111,8 @@ class OverviewMap extends React.Component {
     const position = [60.192059, 24.945831];  // Default to Helsinki's center
     const crs = EPSG3067();
     return (
-      <Map center={position} zoom={9} style={style} minZoom={5} scrollWheelZoom={false} crs={crs}>
+      this.state.height && this.state.width &&
+      <Map center={position} zoom={9} style={{ ...this.state }} minZoom={5} scrollWheelZoom={false} crs={crs}>
         <TileLayer
           url="https://geoserver.hel.fi/mapproxy/wmts/osm-sm-hq/etrs_tm35fin_hq/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -93,7 +127,7 @@ class OverviewMap extends React.Component {
                 [59.9, 24.59],  // SouthWest corner
                 [60.43, 25.3]  // NorthEast corner
               ];  // Wide Bounds of City of Helsinki area
-              input.context.map.setMaxBounds(viewportBounds);
+              // input.context.map.setMaxBounds(viewportBounds);
             }
           }}
         >
