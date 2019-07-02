ac3fd17797ef388964d8bf154017d2204c2c269b
Juha Kujala <juha.kujala@anders.fi>
2019-07-02 16:19:46 +0300

    Fix scrolling issue when routes are changed (#705)
    Currently the page does not scroll to the top if a page gets
    changed. If the user has scrolled down and clicks e.g. navigation
    links, the page will stay scrolled down at the point where user
    was.
    
    This is a fix to scroll up when using routes to go to a new page.
    
    Found the solution from here:
    https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/guides/scroll-restoration.md

--- src/getRoot.js
+++ src/getRoot.js
@@ -3,13 +3,16 @@ import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
 import App from './App';
 import {history} from './createStore';
+import ScrollToTop from './scrollToTop';
 
 export default function getRoot(store) {
   return (
     <div>
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <App />
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
         </ConnectedRouter>
       </Provider>
     </div>
