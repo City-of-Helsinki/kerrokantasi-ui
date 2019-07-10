ae068c8dee08a8a53815e64b45077e01f104fc7f
Riku Oja <riku.oja@kapsi.fi>
2019-07-10 18:54:12 +0300

    Webpack code splitting for different routes

--- conf/webpack/prod.js
+++ conf/webpack/prod.js
@@ -19,6 +19,9 @@ module.exports = merge(common, {
         }
       }),
     ],
+    splitChunks: {
+      chunks: 'async',
+    }
   },
   module: {
     rules: [
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import HearingsContainer from './views/Hearings';
-import HomeContainer from './views/Home';
-import Info from './views/Info';
-import HearingContainer from './views/Hearing/HearingContainer';
-import NewHearingContainer from './views/NewHearing/NewHearingContainer';
-import FullscreenHearingContainer from './views/FullscreenHearing/FullscreenHearingContainer';
+
+const HomeContainer = lazy(() => import('./views/Home'));
+const Info = lazy(() => import('./views/Info'));
+const HearingsContainer = lazy(() => import('./views/Hearings'));
+const HearingContainer = lazy(() => import('./views/Hearing/HearingContainer'));
+const NewHearingContainer = lazy(() => import('./views/NewHearing/NewHearingContainer'));
+const FullscreenHearingContainer = lazy(() => import('./views/FullscreenHearing/FullscreenHearingContainer'));
 
 /* Vanilla Redirect component can't handle dynamic rerouting,
  * so we need Redirector to access params for the hearingSlug
@@ -24,16 +25,18 @@ Redirector.propTypes = {
 };
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={HomeContainer} />
-    <Route path="/info" component={Info} />
-    <Route path="/hearings/:tab" component={HearingsContainer} />
-    <Route path="/hearing/new" component={NewHearingContainer} />
-    <Route path="/hearing/:hearingSlug" component={Redirector} />
-    <Route path="/:hearingSlug/fullscreen" component={FullscreenHearingContainer} />
-    <Route path="/:hearingSlug/:sectionId" component={HearingContainer} />
-    <Route path="/:hearingSlug" component={HearingContainer} />
-  </Switch>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+      <Route exact path="/" component={HomeContainer} />
+      <Route path="/info" component={Info} />
+      <Route path="/hearings/:tab" component={HearingsContainer} />
+      <Route path="/hearing/new" component={NewHearingContainer} />
+      <Route path="/hearing/:hearingSlug" component={Redirector} />
+      <Route path="/:hearingSlug/fullscreen" component={FullscreenHearingContainer} />
+      <Route path="/:hearingSlug/:sectionId" component={HearingContainer} />
+      <Route path="/:hearingSlug" component={HearingContainer} />
+    </Switch>
+  </Suspense>
 );
 
 export default Routes;
