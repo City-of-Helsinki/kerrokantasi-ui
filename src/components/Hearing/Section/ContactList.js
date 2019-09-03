2a0053a56b3c5d178277aca5aa8d4d9ca9d5bdd3
Juha Kujala <juha.kujala@anders.fi>
2019-09-03 08:48:44 +0300

    Update contacts component

--- src/components/Hearing/Section/ContactList.js
+++ src/components/Hearing/Section/ContactList.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 import ContactCard from '../../ContactCard';
-import {FormattedMessage} from 'react-intl';
 
 export const ContactListComponent = ({contacts, language}) => {
   return (
-    <div className="hearing-contacts">
-      <h3>
-        <FormattedMessage id="contactPersons" />
-      </h3>
-      <Row>
-        {contacts &&
-          contacts.map((person, index) => (
-            <Col
-              xs={6}
-              key={index + Math.random()} // eslint-disable-line react/no-array-index-key
-              md={4}
-            >
-              <ContactCard activeLanguage={language} {...person} />
-            </Col>
-          ))}
-      </Row>
-    </div>
+    <Row>
+      {contacts && contacts.map((person, index) => (
+        <ContactCard
+          activeLanguage={language}
+          key={index + Math.random() /* eslint-disable-line react/no-array-index-key */}
+          {...person}
+        />
+      ))}
+    </Row>
   );
 };
 
