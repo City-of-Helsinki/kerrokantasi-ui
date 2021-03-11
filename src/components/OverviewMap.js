00e9d61ca558cf6e2a69b36540b4d731615ebbad
Hannes <50320115+hienous@users.noreply.github.com>
2021-03-11 11:54:35 +0200

    map marker popup added (#63)
    * markers display popup when clicked on the overviewmap, minor refactoring
    
    * removed unnecessary export

--- src/components/OverviewMap.js
+++ src/components/OverviewMap.js
@@ -65,67 +65,24 @@ class OverviewMap extends React.Component {
     this.props.showOnCarousel ? (this.state.height && this.state.width) : true
   );
 
-  getMapElement(geojson, content) {
-    switch (geojson.type) {
-      case "Polygon": {
-        // XXX: This only supports the _first_ ring of coordinates in a Polygon
-        const latLngs = geojson.coordinates[0].map(([lng, lat]) => new LatLng(lat, lng));
-        return (<Polygon key={Math.random()} positions={latLngs}>{content}</Polygon>);
-      }
-      case "Point": {
-        const latLngs = new LatLng(geojson.coordinates[1], geojson.coordinates[0]);
-        return (
-          <Marker
-            position={latLngs}
-            key={Math.random()}
-            icon={new Leaflet.Icon({
-              iconUrl: leafletMarkerIconUrl,
-              shadowUrl: leafletMarkerShadowUrl,
-              iconRetinaUrl: leafletMarkerRetinaIconUrl,
-              iconSize: [25, 41],
-              iconAnchor: [13, 41]
-            })}
-          />
-        );
-      }
-      case "LineString": {
-        const latLngs = geojson.coordinates.map(([lng, lat]) => new LatLng(lat, lng));
-        return (<Polyline key={Math.random()} positions={latLngs}>{content}</Polyline>);
-      }
-      case "Feature": {
-        return (this.getMapElement(geojson.geometry, content));
-      }
-      default:
-        // TODO: Implement support for other geometries too (markers, square, circle)
-        return (<GeoJSON data={geojson} key={JSON.stringify(geojson)}>{content}</GeoJSON>);
-    }
-  }
 
   getHearingMapContent(hearings) {
-    const {language} = this.context;
     const contents = [];
-
     hearings.forEach((hearing) => {
       const {geojson, id} = hearing;
-      const content = (
-        this.props.enablePopups ? (
-          <Popup>
-            <div>
-              <h4>
-                <a href={getHearingURL(hearing)}>{getAttr(hearing.title, language)}</a>
-              </h4>
-              <p>{getAttr(hearing.abstract, language)}</p>
-            </div>
-          </Popup>
-        ) : null
-      );
 
       if (geojson) {
         switch (geojson.type) {
           case "Polygon": {
             // XXX: This only supports the _first_ ring of coordinates in a Polygon
             const latLngs = geojson.coordinates[0].map(([lng, lat]) => new LatLng(lat, lng));
-            contents.push(<Polygon key={Math.random()} positions={latLngs}>{content}</Polygon>);
+            contents.push(
+              <Polygon
+                key={Math.random()}
+                positions={latLngs}
+              >
+                {this.getPopupContent(hearing, geojson)}
+              </Polygon>);
           }
             break;
           case "Point": {
@@ -141,34 +98,119 @@ class OverviewMap extends React.Component {
                   iconSize: [25, 41],
                   iconAnchor: [13, 41]
                 })}
-              />
+              >{this.getPopupContent(hearing, geojson)}
+              </Marker>
             );
           }
             break;
           case "LineString": {
             const latLngs = geojson.coordinates.map(([lng, lat]) => new LatLng(lat, lng));
-            contents.push(<Polyline key={Math.random()} positions={latLngs}>{content}</Polyline>);
+            contents.push(
+              <Polyline key={Math.random()} positions={latLngs}>{this.getPopupContent(hearing, geojson)}</Polyline>
+            );
           }
             break;
           // eslint-disable-next-line no-lone-blocks
           case "FeatureCollection": {
             if (this.props.disableCollections) {
-              contents.push(this.getMapElement(geojson.features[0].geometry, content));
+              contents.push(this.getMapElement(geojson.features[0].geometry, hearing));
             } else {
               geojson.features.forEach((feature) => {
-                contents.push(this.getMapElement(feature, content));
+                contents.push(this.getMapElement(feature, hearing));
               });
             }
           }
             break;
           default:
           // TODO: Implement support for other geometries too (markers, square, circle)
-            contents.push(<GeoJSON data={geojson} key={JSON.stringify(geojson)}>{content}</GeoJSON>);
+            contents.push(
+              <GeoJSON data={geojson} key={JSON.stringify(geojson)}>{this.getPopupContent(hearing, geojson)}</GeoJSON>
+            );
         }
         // contents.push(<GeoJSON key={id} data={geojson}>{content}</GeoJSON>);
       }
     });
     return contents;
+  }
+
+  /**
+   * Return Map element based on geojson.type.
+   * @param {Object} geojson
+   * @param {Object} hearing
+   * @returns {JSX.Element|*}
+   */
+  getMapElement(geojson, hearing) {
+    switch (geojson.type) {
+      case "Polygon": {
+        // XXX: This only supports the _first_ ring of coordinates in a Polygon
+        const latLngs = geojson.coordinates[0].map(([lng, lat]) => new LatLng(lat, lng));
+        return (
+          <Polygon
+            key={Math.random()}
+            positions={latLngs}
+          >
+            {this.getPopupContent(hearing, geojson)}
+          </Polygon>);
+      }
+      case "Point": {
+        const latLngs = new LatLng(geojson.coordinates[1], geojson.coordinates[0]);
+        return (
+          <Marker
+            position={latLngs}
+            key={Math.random()}
+            icon={new Leaflet.Icon({
+              iconUrl: leafletMarkerIconUrl,
+              shadowUrl: leafletMarkerShadowUrl,
+              iconRetinaUrl: leafletMarkerRetinaIconUrl,
+              iconSize: [25, 41],
+              iconAnchor: [13, 41]
+            })}
+          >{this.getPopupContent(hearing, geojson)}
+          </Marker>
+        );
+      }
+      case "LineString": {
+        const latLngs = geojson.coordinates.map(([lng, lat]) => new LatLng(lat, lng));
+        return (<Polyline key={Math.random()} positions={latLngs}>{this.getPopupContent(hearing, geojson)}</Polyline>);
+      }
+      case "Feature": {
+        /**
+         * Recursively get the Map element
+         */
+        return (this.getMapElement(geojson.geometry, hearing));
+      }
+      default:
+        // TODO: Implement support for other geometries too (markers, square, circle)
+        return (
+          <GeoJSON data={geojson} key={JSON.stringify(geojson)}>{this.getPopupContent(hearing, geojson)}</GeoJSON>
+        );
+    }
+  }
+
+  /**
+   * Return Popup with content based on hearing. If geojson.type is 'Point', apply offset to Popup
+   * @param {Object} hearing
+   * @param {Object} geojson
+   * @returns {JSX.Element|null}
+   */
+  getPopupContent(hearing, geojson) {
+    const {language} = this.context;
+    const {enablePopups} = this.props;
+    const options = geojson.type === 'Point' ? {offset: [0, -20]} : {};
+    if (enablePopups) {
+      const hearingURL = getHearingURL(hearing) + document.location.search;
+      return (
+        <Popup {...options}>
+          <div>
+            <h4>
+              <a href={hearingURL}>{getAttr(hearing.title, language)}</a>
+            </h4>
+            <p>{getAttr(hearing.abstract, language)}</p>
+          </div>
+        </Popup>
+      );
+    }
+    return null;
   }
 
   render() {
@@ -180,7 +222,14 @@ class OverviewMap extends React.Component {
     }
     return (
       this.shouldMapRender() &&
-      <Map center={localization.mapPosition} zoom={10} style={{ ...this.state }} minZoom={8} scrollWheelZoom={false} {...this.props.mapSettings}>
+      <Map
+        center={localization.mapPosition}
+        zoom={10}
+        style={{ ...this.state }}
+        minZoom={8}
+        scrollWheelZoom={false}
+        {...this.props.mapSettings}
+      >
         <TileLayer
           url={getCorrectContrastMapTileUrl(urls.rasterMapTiles,
             urls.highContrastRasterMapTiles, this.props.isHighContrast)}
