517e5903ecb1609ae72b6da32495c6c31195a896
joonas.nislin <joonas.nislin@hotmail.com>
2021-04-14 15:59:14 +0300

    Geojson support added for MultiPolygon types

--- src/components/admin/HearingFormStep3.js
+++ src/components/admin/HearingFormStep3.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {injectIntl, FormattedMessage} from 'react-intl';
-import Leaflet from 'leaflet';
+import Leaflet, { latLng } from 'leaflet';
 import getTranslatedTooltips from '../../utils/getTranslatedTooltips';
 import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
+import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 import {isEmpty, includes, keys} from 'lodash';
 import {ZoomControl} from 'react-leaflet';
 import {localizedNotifyError} from '../../utils/notify';
@@ -46,6 +47,12 @@ function getHearingArea(hearing) {
       // XXX: This only supports the _first_ ring of coordinates in a Polygon
       const latLngs = geojson.coordinates[0].map(([lng, lat]) => new LatLng(lat, lng));
       return <Polygon positions={latLngs}/>;
+    }
+    case "MultiPolygon": {
+      const latLngs = geojson.coordinates.map((arr) => {
+        return arr[0].map(([lng, lat]) => new LatLng(lat, lng))
+      });
+      return latLngs.map((latLng, index) => <Polygon key={index} positions={latLng}/>);
     }
     case "Point": {
       const latLngs = new LatLng(geojson.coordinates[1], geojson.coordinates[0]);
@@ -143,6 +150,10 @@ class HearingFormStep3 extends React.Component {
           includes(keys(featureCollection.features[0].geometry), 'type') &&
           includes(keys(featureCollection.features[0].geometry), 'coordinates')
         ) {
+          if (featureCollection.features[0].geometry.coordinates.length == 0) {
+            localizedNotifyError('Tiedostosta ei löytynyt koordinaatteja.');
+            return
+          }
           this.props.onHearingChange("geojson", featureCollection.features[0].geometry);
         } else {
           localizedNotifyError('Virheellinen tiedosto.');
@@ -257,6 +268,7 @@ class HearingFormStep3 extends React.Component {
             <Icon className="icon" name="upload" style={{marginRight: '5px'}}/>
             <FormattedMessage id="addGeojson"/>
           </label>
+          <HelpBlock><FormattedMessage id="addGeojsonInfo"/></HelpBlock>
         </div>
         <div className="step-footer">
           <Button
