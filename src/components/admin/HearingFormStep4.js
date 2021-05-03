29efa04c801f96ea89ec6259c758fd901b755eb3
joonas.nislin <joonas.nislin@hotmail.com>
2021-05-03 10:02:09 +0300

    Closure info content is now saved on create and edit

--- src/components/admin/HearingFormStep4.js
+++ src/components/admin/HearingFormStep4.js
@@ -35,9 +35,8 @@ class HearingFormStep4 extends React.Component {
   onClosureSectionChange(value) {
     const {hearing, onSectionChange, dispatch} = this.props;
     const closureInfoSection = getClosureSection(hearing);
-
     if (closureInfoSection) {
-      onSectionChange(closureInfoSection.id, 'content', value);
+      onSectionChange(closureInfoSection.frontId, 'content', value);
     } else {
       dispatch(addSection(initNewSection({type: SectionTypes.CLOSURE, content: value})));
     }
