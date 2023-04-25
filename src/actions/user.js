0b2c511ba8ed5021d266d1f4ffdd865e8310c57b
Arto Pärssinen <arto.parssinen@gofore.com>
2023-04-25 15:27:33 +0300

    Fixed wrong default value, userShape expects type of Array

--- src/actions/user.js
+++ src/actions/user.js
@@ -18,7 +18,7 @@ export default function retrieveUserFromSession() {
         {nickname: get(democracyUserJSON, 'nickname')},
         {answered_questions: get(democracyUserJSON, 'answered_questions')},
         {favorite_hearings: get(democracyUserJSON, 'followed_hearings')},
-        {adminOrganizations: get(democracyUserJSON, 'admin_organizations', null)},
+        {adminOrganizations: get(democracyUserJSON, 'admin_organizations', [])},
         {hasStrongAuth: get(democracyUserJSON, 'has_strong_auth', false)});
       return dispatch(createAction('receiveUserData')(userWithOrganization));
     });
