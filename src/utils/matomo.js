665092fa78b16bb0ecc224f57652b5bb8f505ced
Arto Pärssinen <arto.parssinen@gofore.com>
2023-06-14 13:44:18 +0300

    WIP: Test Branch for DevMatomo

--- src/utils/matomo.js
+++ src/utils/matomo.js
@@ -7,8 +7,8 @@ const setupMatomo = () => {
   _paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
 
   //_paq.push(['setCookieDomain', '*.kerrokantasi.hel.fi']);
-  _paq.push(['setCookieDomain', config.matomoCookieDomain]);
-  _paq.push(['setDomains', config.matomoDomains]);
+  _paq.push(['setCookieDomain', '*.kerrokantasi.dev.hel.ninja']);
+  _paq.push(['setDomains', ["*.kerrokantasi.dev.hel.ninja", "*.kerrokantasi.dev.hel.ninja"]]);
   _paq.push(['setDoNotTrack', true]);
   _paq.push(['disableCookies']);
   _paq.push(['trackPageView']);
@@ -16,13 +16,13 @@ const setupMatomo = () => {
   (function () {
     
     
-    var u = config.matomoScriptUrl;
-    _paq.push(['setTrackerUrl', u + 'tracker.php']);
-    _paq.push(['setSiteId', config.matomoSiteId]);
+    var u = '//matomo.dev.hel.ninja/';
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', 3]);
     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
     g.type = 'text/javascript';
     g.async = true;
-    g.src = u + config.matomoScriptFilename;
+    g.src = u + 'matomo.js';
     if(s) {
       s.parentNode.insertBefore(g, s);
     }
