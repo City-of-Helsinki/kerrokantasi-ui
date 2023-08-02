a7d63774f9ea622767954a72ad1c8f68730f3730
mikkojamG <mikko.jamia@gofore.com>
2023-08-02 15:15:46 +0300

    KER-212 Style fixes

--- src/components/HearingsSearch.jsx
+++ src/components/HearingsSearch.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import { FormGroup, ControlLabel, Button } from 'react-bootstrap';
 import { FormattedMessage, intlShape } from 'react-intl';
 import Select from 'react-select';
 import { isEmpty } from 'lodash';
+import { SearchInput } from 'hds-react';
 
 import getAttr from '../utils/getAttr';
 import { labelShape } from '../types';
 import InternalLink from './InternalLink';
 
-class HearingsSearch extends React.Component {
-  render() {
-    const { handleSearch, handleSelectLabels, labels, language, searchPhrase, selectedLabels, intl } = this.props;
+const HearingsSearch = ({ handleSearch, handleSelectLabels, labels, language, searchPhrase, selectedLabels, intl }) => {
+  const [searchValue, setSearchValue] = useState(searchPhrase);
 
-    const labelsAsOptions = labels.map(({ label, id }) => ({
-      label: getAttr(label, language),
-      value: getAttr(label, language),
-      id,
-    }));
+  const labelsAsOptions = labels.map(({ label, id }) => ({
+    label: getAttr(label, language),
+    value: getAttr(label, language),
+    id,
+  }));
 
-    return (
-      <div className='hearings-search__container'>
-        <div className='hearings-search__content'>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
-              handleSearch(this.input.value, true);
-            }}
-            id='hearings-search-form'
-          >
+  return (
+    <div className='hearings-search__container'>
+      <div className='hearings-search__content'>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleSearch(searchValue);
+          }}
+          id='hearings-search-form'
+        >
+          <div className='hearings-search-input-container'>
             <FormGroup className='hearings-search__text' controlId='formControlsSearchText'>
-              <ControlLabel>
-                <FormattedMessage id='searchTitles' />
-              </ControlLabel>
-              <FormControl
-                type='text'
-                inputRef={(ref) => {
-                  this.input = ref;
+              <SearchInput
+                label={<FormattedMessage id='searchTitles' />}
+                searchButtonAriaLabel={<FormattedMessage id='searchTitles' />}
+                clearButtonAriaLabel='Clear'
+                value={searchValue}
+                onChange={(newValue) => setSearchValue(newValue)}
+                onSubmit={(value) => {
+                  handleSearch(value);
                 }}
-                defaultValue={searchPhrase}
-                onBlur={() => handleSearch(this.input.value)}
               />
             </FormGroup>
             <FormGroup className='hearings-search__label' controlId='formControlsSearchSelect'>
@@ -57,18 +57,18 @@ class HearingsSearch extends React.Component {
                 />
               )}
             </FormGroup>
-            <Button className='hearings-search__button' bsStyle='primary' type='submit'>
-              <FormattedMessage id='search' />
-            </Button>
-            <InternalLink destinationId='hearings-section' srOnly>
-              <FormattedMessage id='jumpToSearchResults' />
-            </InternalLink>
-          </form>
-        </div>
+          </div>
+          <Button className='hearings-search__button' bsStyle='primary' type='submit'>
+            <FormattedMessage id='search' />
+          </Button>
+          <InternalLink destinationId='hearings-section' srOnly>
+            <FormattedMessage id='jumpToSearchResults' />
+          </InternalLink>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 HearingsSearch.propTypes = {
   handleSearch: PropTypes.func,
