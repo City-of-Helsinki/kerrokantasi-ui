6fb9cd3974efbdeafa10f46b29f7dfb29bcb8f7c
Arto Pärssinen <arto.parssinen@gofore.com>
2023-09-04 11:56:18 +0300

    fix: Fixed docker build not working with new webpack configurations

--- conf/webpack/prod.js
+++ conf/webpack/prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 const common = require('./common');
@@ -11,10 +11,9 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [
       new TerserPlugin({
-        cache: true,
         parallel: true,
-        sourceMap: true, // Must be set to true if using source-maps in production
         terserOptions: {
+          sourceMap: true, // Must be set to true if using source-maps in production
           // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
         }
       }),
@@ -26,17 +25,14 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
           {
-            loaders: ['babel-loader'],
-            options: {
-              presets: ['react']
-            }
+            loader: 'babel-loader'
           }
         ]
-      },
+      }
     ],
   },
 });
