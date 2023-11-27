e4c393a604a89f355cd8e8c04aecdc51087143d2
Arto Pärssinen <arto.parssinen@gofore.com>
2023-11-27 15:28:06 +0200

    feat: added transitions for bar on entry to viewport

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 function ProgressBar(props) {
+    const [isIntersecting, setIsIntersecting] = useState(false);
 
+    const [barWidth, setBarWidth] = useState(0);
     const styleprops = {
-        width: props.now + '%'
+        width: barWidth + '%'
     }
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsIntersecting(entry.isIntersecting);
+            }
+        );
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, [isIntersecting])
+
+    useEffect(() => {
+        if (isIntersecting) {
+          ref.current.querySelectorAll("div").forEach((el) => {
+            el.classList.add("in-viewport");
+            setBarWidth(props.now);
+          });
+        }
+      }, [isIntersecting]);
+
     return (
-        <div className="progressBar-wrapper">
+        <div className="progressBar-wrapper" ref={ref}>
             <div 
                 className="progressBar"
                 aria-valuenow={props.now}
