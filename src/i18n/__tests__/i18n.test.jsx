f6426f38a4cd0940fc182a167d025e210b35116d
Arto Pärssinen <arto.parssinen@gofore.com>
2024-03-07 13:32:09 +0200

    fix: linter fixes let to const

--- src/i18n/__tests__/i18n.test.jsx
+++ src/i18n/__tests__/i18n.test.jsx
@@ -8,19 +8,19 @@ describe('Translation files should have matching keys', () => {
     const swedishKeys = Object.keys(swedishTranslations);
     const englishKeys = Object.keys(englishTranslations);
     test('swedish translations has all same keys as finnish translations', () => {
-        let missingKeys = finnishKeys.filter(x => !swedishKeys.includes(x));
+        const missingKeys = finnishKeys.filter(x => !swedishKeys.includes(x));
         expect(missingKeys).toEqual([]);
     })
     test('swedish translations does not have extra keys compared to finnish translations', () => {
-        let extraKeys = swedishKeys.filter(x => !finnishKeys.includes(x));
+        const extraKeys = swedishKeys.filter(x => !finnishKeys.includes(x));
         expect(extraKeys).toEqual([]);
     })
     test('english translations has all same keys as finnish translations', () => {
-        let missingKeys = finnishKeys.filter(x => !englishKeys.includes(x));
+        const missingKeys = finnishKeys.filter(x => !englishKeys.includes(x));
         expect(missingKeys).toEqual([]);
     })
     test('english translations does not have extra keys compared to finnish translations', () => {
-        let extraKeys = englishKeys.filter(x => !finnishKeys.includes(x));
+        const extraKeys = englishKeys.filter(x => !finnishKeys.includes(x));
         expect(extraKeys).toEqual([]);
     })
 });
