d79ab375fa4619d02e10440df6dd15ca0313a80b
Arto Pärssinen <arto.parssinen@gofore.com>
2024-03-27 13:50:11 +0200

    fix: fixed broken footer links for all non-finnish languages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ import settings from '@city-assets/settings.json';
 import { Footer as HDSFooter, Logo } from 'hds-react';
 import logoSwedishWhite from '@city-images/logo-sv-white.svg';
 import logoWhite from '@city-images/logo-fi-white.svg';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
-import { getFeedbackEmailUrl, getFeedbackUrl } from '../utils/languageUtils';
+import { getDataProtectionUrl, getFeedbackEmailUrl, getFeedbackUrl } from '../utils/languageUtils';
 import config from '../config';
 import { getUser } from '../selectors/user';
 import { isAdmin } from '../utils/user';
@@ -65,17 +65,17 @@ const Footer = (props) => {
           />
         }
       >
-        <HDSFooter.Link label={<FormattedMessage id='accessibilityLink' />} href='/accessibility' />
+        <HDSFooter.Link label={<FormattedMessage id='accessibilityLink' />} to={"/accessibility?lang="+language} as={Link} />
         <HDSFooter.Link
           label={<FormattedMessage id='dataProtection' />}
-          href={urls.dataProtection}
+          href={getDataProtectionUrl(language)}
           target='_blank'
           rel='noopener noreferrer'
         />
         {config.enableCookies && (
-          <HDSFooter.Link label={<FormattedMessage id='cookieManagementLink' />} href='/cookies' />
+          <HDSFooter.Link label={<FormattedMessage id='cookieManagementLink' />} to={"/cookies?lang="+language} as={Link} />
         )}
-        <HDSFooter.Link label={<FormattedMessage id='infoHeaderText' />} href='/info' />
+        <HDSFooter.Link label={<FormattedMessage id='infoHeaderText' />} to={"/info?lang="+language} as={Link} />
       </HDSFooter.Base>
     </HDSFooter>
   );
