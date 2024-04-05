910f917609a3bf51f53e66764bd1c375ed175a1c
Arto Pärssinen <arto.parssinen@gofore.com>
2024-04-05 12:46:05 +0300

    fix: fixing lint issues

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -4,13 +4,11 @@ import { MemoryRouter } from 'react-router-dom';
 import renderWithProviders from '../utils/renderWithProviders';
 import App from '../App';
 
-const renderComponent = (props) => {
-  return renderWithProviders(
+const renderComponent = (props) => renderWithProviders(
     <MemoryRouter>
       <App {...props} />
     </MemoryRouter>,
   );
-} 
 
 const {container, store} = renderComponent();
 
