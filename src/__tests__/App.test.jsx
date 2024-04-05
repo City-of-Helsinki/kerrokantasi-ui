b72b23d92e98292df06367c72eb979cd1ab3e250
Arto Pärssinen <arto.parssinen@gofore.com>
2024-04-05 12:43:35 +0300

    fix: adding tests for coverage

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -4,12 +4,23 @@ import { MemoryRouter } from 'react-router-dom';
 import renderWithProviders from '../utils/renderWithProviders';
 import App from '../App';
 
+const renderComponent = (props) => {
+  return renderWithProviders(
+    <MemoryRouter>
+      <App {...props} />
+    </MemoryRouter>,
+  );
+} 
+
+const {container, store} = renderComponent();
+
 describe('<App />', () => {
   it('renders correctly', () => {
-    renderWithProviders(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    expect(container).toBeTruthy();
+  });
+
+  it('has correct state on initial load', () => {
+    const state = store.getState();
+    expect(state.intervalSet).toBeFalsy();
   });
 });
