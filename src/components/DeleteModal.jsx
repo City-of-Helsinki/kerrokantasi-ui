d9bfe10453dc263cc8b52e23c9f8b8cccc9e88b1
mikkojamG <mikko.jamia@gofore.com>
2024-04-16 09:39:18 +0300

    feat: delete modal replace with HDS dialog KER-298

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'react-bootstrap';
-import { Button } from 'hds-react';
+import { Button, Dialog } from 'hds-react';
 import { FormattedMessage } from 'react-intl';
 
-const DeleteModal = ({ isOpen, close, onDeleteComment }) => (
-  <Modal className='delete-modal' show={isOpen} onHide={() => close()} animation={false}>
-    <Modal.Header closeButton>
-      <Modal.Title>
-        <FormattedMessage id='deleteConfirmation' />
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Footer>
-      <Button className="kerrokantasi-btn" onClick={() => close()}>
-        <FormattedMessage id='cancel' />
-      </Button>
-      <Button
-        className="kerrokantasi-btn danger"
-        onClick={() => {
-          onDeleteComment();
-          close();
-        }}
-      >
-        <FormattedMessage id='deleteComment' />
-      </Button>
-    </Modal.Footer>
-  </Modal>
-);
+const DeleteModal = ({ isOpen, close, onDeleteComment }) => {
+  const titleId = 'delete-modal-title';
+  const descriptionId = 'delete-modal-description';
+
+  return (
+    <Dialog isOpen={isOpen} close={close} aria-labelledby={titleId} aria-describedby={descriptionId}>
+      <Dialog.Header id={titleId} title={<FormattedMessage id='deleteComment' />} />
+      <Dialog.Content>
+        <span id={descriptionId}>
+          <FormattedMessage id='deleteConfirmation' />
+        </span>
+      </Dialog.Content>
+      <Dialog.ActionButtons>
+        <Button className='kerrokantasi-btn' onClick={() => close()}>
+          <FormattedMessage id='cancel' />
+        </Button>
+        <Button
+          className='kerrokantasi-btn danger'
+          onClick={() => {
+            onDeleteComment();
+            close();
+          }}
+        >
+          <FormattedMessage id='deleteComment' />
+        </Button>
+      </Dialog.ActionButtons>
+    </Dialog>
+  );
+};
 
 DeleteModal.propTypes = {
   isOpen: PropTypes.bool,
