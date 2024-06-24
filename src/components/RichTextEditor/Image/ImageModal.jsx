178db62e0f1840d2623ba31e78ee0ded65d20f97
mikkojamG <mikko.jamia@gofore.com>
2024-06-24 12:37:55 +0300

    chore: modals don't import intlShape KER-363

--- src/components/RichTextEditor/Image/ImageModal.jsx
+++ src/components/RichTextEditor/Image/ImageModal.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import { ControlLabel, HelpBlock, Image } from 'react-bootstrap';
 import { Button, Dialog } from 'hds-react';
@@ -122,7 +123,7 @@ class ImageModal extends React.Component {
                   </>
                 )
               }
-              
+
             </Dropzone>
             <HelpBlock>
               <FormattedMessage id='sectionImageHelpText' />
@@ -158,7 +159,7 @@ class ImageModal extends React.Component {
 
 ImageModal.propTypes = {
   isOpen: PropTypes.bool,
-  intl: intlShape.isRequired,
+  intl: PropTypes.object,
   onClose: PropTypes.func,
   onSubmit: PropTypes.func,
 };
