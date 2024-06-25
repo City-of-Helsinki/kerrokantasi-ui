4435c2f9ba1b4c0f7334d33c02984ec5363fc70e
mikkojamG <mikko.jamia@gofore.com>
2024-06-25 11:56:58 +0300

    chore: github ci no need to run test coverage KER-363

--- src/__mocks__/reactLeafletMock.jsx
+++ src/__mocks__/reactLeafletMock.jsx
@@ -9,6 +9,8 @@ const TileLayer = () => <div data-testId='TileLayer' />;
 
 const Marker = ({ children }) => <div data-testId='Marker'>{children}</div>;
 
+const FeatureGroup = ({ children }) => <div data-testId='FeatureGroup'>{children}</div>;
+
 const Popup = () => <div data-testId='Popup' />;
 
-export { MapContainer, TileLayer, Marker, Popup, useMap };
+export { MapContainer, TileLayer, Marker, FeatureGroup, Popup, useMap };
