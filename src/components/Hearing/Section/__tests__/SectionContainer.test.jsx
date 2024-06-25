5027f8d085969d3852c54d51aa41b3c129066cc1
mikkojamG <mikko.jamia@gofore.com>
2024-06-25 15:42:06 +0300

    chore: section container tests working KER-363

--- src/components/Hearing/Section/__tests__/SectionContainer.test.jsx
+++ src/components/Hearing/Section/__tests__/SectionContainer.test.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 import SectionContainerComponent from '../SectionContainer';
 import { mockStore, getIntlAsProp } from '../../../../../test-utils';
 import renderWithProviders from '../../../../utils/renderWithProviders';
+import createAppStore from '../../../../createStore';
+
+const { labels, sectionComments, mockHearingWithSections, user } = mockStore;
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: `/${mockHearingWithSections.data.id}`,
+    search: '',
+  }),
+  useParams: () => ({
+    hearingSlug: mockHearingWithSections.data.id,
+  }),
+}));
 
 const renderComponent = (propOverrides) => {
-  const {
-    labels,
-    sectionComments,
-    hearingLists: { allHearings },
-    mockHearingWithSections,
-    user,
-  } = mockStore;
-
   const props = {
     labels: labels.data,
     hearing: mockHearingWithSections.data,
     hearingDraft: {},
-    match: {
-      params: {
-        hearingSlug: allHearings.data[0].slug,
-      },
-    },
-    location: {
-      pathname: `/${allHearings.data[0].slug}`,
-    },
     sectionComments,
     showClosureInfo: true,
     sections: mockHearingWithSections.data.sections,
@@ -36,10 +35,20 @@ const renderComponent = (propOverrides) => {
     ...propOverrides,
   };
 
+  const history = createMemoryHistory();
+  const storeMock = createAppStore({
+    hearing: {
+      [mockHearingWithSections.data.id]: {
+        ...mockHearingWithSections,
+      },
+    },
+  });
+
   return renderWithProviders(
     <MemoryRouter>
       <SectionContainerComponent intl={getIntlAsProp()} {...props} />
     </MemoryRouter>,
+    { store: storeMock, history },
   );
 };
 
