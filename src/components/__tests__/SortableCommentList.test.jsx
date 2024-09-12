d85018b8ee30f99105ad392a50d4d42d51a2b6e3
mikkojamG <mikko.jamia@gofore.com>
2024-09-12 12:20:20 +0300

    chore: sortableCommentList to function KER-346

--- src/components/__tests__/SortableCommentList.test.jsx
+++ src/components/__tests__/SortableCommentList.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { SortableCommentListComponent } from '../SortableCommentList';
+import SortableCommentListComponent from '../SortableCommentList';
 import { mockStore, getIntlAsProp } from '../../../test-utils';
 import renderWithProviders from '../../utils/renderWithProviders';
 
