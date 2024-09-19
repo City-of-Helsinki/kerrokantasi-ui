1d5416654e5a63a110c27332a47935a9b89b0ad1
mikkojamG <mikko.jamia@gofore.com>
2024-09-19 09:27:45 +0300

    chore: hearingListFilters to function KER-346

--- src/components/HearingList/HearingListFilters/HearingListFilters.jsx
+++ src/components/HearingList/HearingListFilters/HearingListFilters.jsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Select } from 'hds-react';
 import { FormattedMessage } from 'react-intl';
 import { uniqueId } from 'lodash';
 
-class HearingListFilters extends React.Component {
-  constructor() {
-    super();
+const HearingListFilters = ({ handleSort, formatMessage }) => {
+  const [sortChangeStatusMessages, setSortChangeStatusMessages] = useState([]);
 
-    this.state = {
-      sortChangeStatusMessages: [],
-    };
-  }
-
-  sortList = (value) => {
-    const { handleSort } = this.props;
+  const sortList = (value) => {
     const sortBy = value.replace('_from_open', '').replace('_from_closed', '');
     const showOnlyOpen = value.indexOf('_from_open') !== -1;
     const showOnlyClosed = value.indexOf('_from_closed') !== -1;
@@ -25,48 +18,44 @@ class HearingListFilters extends React.Component {
       id: uniqueId(),
     };
 
-    this.setState({ sortChangeStatusMessages: [newMessage] });
+    setSortChangeStatusMessages([newMessage]);
 
     // Clear the message after 5 seconds
     setTimeout(() => {
-      this.setState({ sortChangeStatusMessages: [] });
+      setSortChangeStatusMessages([]);
     }, 5000);
   };
 
-  render() {
-    const { formatMessage } = this.props;
+  const sortOptions = [
+    { value: '-created_at', label: formatMessage({ id: 'newestFirst' }) },
+    { value: 'created_at', label: formatMessage({ id: 'oldestFirst' }) },
+    { value: '-close_at_from_open', label: formatMessage({ id: 'lastClosing' }) },
+    { value: 'close_at_from_open', label: formatMessage({ id: 'firstClosing' }) },
+    { value: '-close_at_from_closed', label: formatMessage({ id: 'lastClosed' }) },
+    { value: '-n_comments', label: formatMessage({ id: 'mostCommented' }) },
+    { value: 'n_comments', label: formatMessage({ id: 'leastCommented' }) },
+  ];
 
-    const sortOptions = [
-      { value: '-created_at', label: formatMessage({ id: 'newestFirst' }) },
-      { value: 'created_at', label: formatMessage({ id: 'oldestFirst' }) },
-      { value: '-close_at_from_open', label: formatMessage({ id: 'lastClosing' }) },
-      { value: 'close_at_from_open', label: formatMessage({ id: 'firstClosing' }) },
-      { value: '-close_at_from_closed', label: formatMessage({ id: 'lastClosed' }) },
-      { value: '-n_comments', label: formatMessage({ id: 'mostCommented' }) },
-      { value: 'n_comments', label: formatMessage({ id: 'leastCommented' }) },
-    ];
-
-    return (
-      <div className='hearing-list__filter-bar clearfix'>
-        <div className='sr-only'>
-          {this.state.sortChangeStatusMessages?.map((alert) => (
-            <div key={alert.id} aria-live='assertive' role='status'>
-              <FormattedMessage id='orderHasBeenChanged' />
-            </div>
-          ))}
-        </div>
-        <div id='formControlsSelect' className='hearing-list__filter-bar-filter'>
-          <Select
-            label={<FormattedMessage id='sort' />}
-            onChange={(selected) => this.sortList(selected.value)}
-            defaultValue={sortOptions[0]}
-            options={sortOptions}
-          />
-        </div>
+  return (
+    <div className='hearing-list__filter-bar clearfix'>
+      <div className='sr-only'>
+        {sortChangeStatusMessages?.map((alert) => (
+          <div key={alert.id} aria-live='assertive' role='status'>
+            <FormattedMessage id='orderHasBeenChanged' />
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+      <div id='formControlsSelect' className='hearing-list__filter-bar-filter'>
+        <Select
+          label={<FormattedMessage id='sort' />}
+          onChange={(selected) => sortList(selected.value)}
+          defaultValue={sortOptions[0]}
+          options={sortOptions}
+        />
+      </div>
+    </div>
+  );
+};
 
 HearingListFilters.propTypes = {
   handleSort: PropTypes.func,
