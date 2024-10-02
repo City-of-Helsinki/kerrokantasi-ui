ca4503076bc00731fefc127b8393cfcabb026afe
mikkojamG <mikko.jamia@gofore.com>
2024-10-02 12:13:14 +0300

    chore: hearingForm tests KER-346

---
+++ src/components/admin/__tests__/HearingForm.test.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import { thunk } from 'redux-thunk';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
+
+import HearingForm from '../HearingForm';
+import renderWithProviders from '../../../utils/renderWithProviders';
+import { mockStore as mockData } from '../../../../test-utils';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const storeInitialState = {
+  projectLists: {
+    isFetching: false,
+    data: [
+      {
+        id: '123',
+        title: {
+          en: 'en',
+          fi: 'fi',
+          sv: 'sv',
+        },
+        phases: [
+          {
+            id: '3456',
+            has_hearings: true,
+            hearings: ['test'],
+            title: {
+              en: 'en',
+              fi: 'fi',
+              sv: 'sv',
+            },
+            description: {
+              en: 'test',
+              fi: 'test',
+              sv: 'test',
+            },
+            schedule: {
+              en: '2024',
+              fi: '2024',
+              sv: '2024',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  accessibility: { isHighContrast: false },
+};
+
+const renderComponent = (propOverrides, storeOverride) => {
+  const { mockHearingWithSections } = mockData;
+
+  const props = {
+    contactPersons: mockHearingWithSections.data.contact_persons,
+    currentStep: 1,
+    organizations: [],
+    intl: { formatMessage: jest.fn((msg) => msg.id) },
+    editorMetaData: {},
+    hearing: { ...mockHearingWithSections.data, published: false },
+    isSaving: false,
+    labels: [],
+    language: 'en',
+    hearingLanguages: [],
+    dispatch: jest.fn(),
+    show: true,
+    sectionMoveUp: jest.fn(),
+    sectionMoveDown: jest.fn(),
+    addOption: jest.fn(),
+    deleteOption: jest.fn(),
+    onQuestionChange: jest.fn(),
+    onDeleteTemporaryQuestion: jest.fn(),
+    clearQuestions: jest.fn(),
+    initMultipleChoiceQuestion: jest.fn(),
+    initSingleChoiceQuestion: jest.fn(),
+    onAddMapMarker: jest.fn(),
+    onAddMapMarkersToCollection: jest.fn(),
+    onCreateMapMarker: jest.fn(),
+    onDeleteExistingQuestion: jest.fn(),
+    onHearingChange: jest.fn(),
+    onLanguagesChange: jest.fn(),
+    onSectionAttachment: jest.fn(),
+    onSectionAttachmentDelete: jest.fn(),
+    onSectionChange: jest.fn(),
+    onSectionImageChange: jest.fn(),
+    onSaveChanges: jest.fn(),
+    onSaveAsCopy: jest.fn(),
+    onSaveAndPreview: jest.fn(),
+    onLeaveForm: jest.fn(),
+    errors: {},
+    ...propOverrides,
+  };
+
+  const store = storeOverride || mockStore(storeInitialState);
+
+  return renderWithProviders(<HearingForm {...props} />, { store });
+};
+
+describe('<HearingForm />', () => {
+  it('should render correctly', () => {
+    renderComponent();
+  });
+
+  it('should call onLeaveForm when cancel button is clicked', () => {
+    const onLeaveForm = jest.fn();
+
+    renderComponent({ onLeaveForm });
+
+    fireEvent.click(screen.getByText('cancel'));
+
+    expect(onLeaveForm).toHaveBeenCalled();
+  });
+
+  it('should call onSaveAndPreview when save and preview button is clicked', () => {
+    const onSaveAndPreview = jest.fn();
+
+    renderComponent({ onSaveAndPreview });
+
+    fireEvent.click(screen.getByText('saveAndPreviewHearing'));
+
+    expect(onSaveAndPreview).toHaveBeenCalled();
+  });
+
+  it('should display errors if present', () => {
+    const errors = {
+      1: { title: 'Fill in title', slug: 'Fill in address' },
+      5: { project_title: 'Fill in project title' },
+    };
+
+    renderComponent({ errors });
+
+    expect(screen.getByText('saveFailed')).toBeInTheDocument();
+  });
+
+  it('should call onToggleClick when accordion is clicked', async () => {
+    renderComponent();
+
+    fireEvent.click(screen.getByText('hearingFormHeaderStep2'));
+
+    await waitFor(() => {
+      expect(screen.getByText('mainSection')).toBeInTheDocument();
+    });
+  });
+
+  it('should navigate to next step when onContinue is called', async () => {
+    renderComponent();
+
+    fireEvent.click(screen.getAllByText('hearingFormNext')[0]);
+
+    await waitFor(() => {
+      expect(screen.getByText('hearingFormHeaderStep2')).toBeInTheDocument();
+    });
+  });
+});
