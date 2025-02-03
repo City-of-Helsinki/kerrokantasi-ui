0f8e57926e923194fc6fa99f6292453ff483e4d8
mikkojamG <mikko.jamia@gofore.com>
2025-02-03 11:55:40 +0200

    chore: fileToDataUri tests KER-422

---
+++ src/utils/images/__tests__/fileToDataUri.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable func-names */
+import fileToDataUri from '../fileToDataUri';
+
+describe('fileToDataUri', () => {
+  it('should resolve to a data URI string when file reading is successful', async () => {
+    const mockFile = new Blob(['file content'], { type: 'text/plain' });
+    const expectedDataUri = 'data:text/plain;base64,ZmlsZSBjb250ZW50';
+
+    const result = await fileToDataUri(mockFile);
+
+    expect(result).toBe(expectedDataUri);
+  });
+
+  it('should reject with an error when file reading fails', async () => {
+    const mockFile = new Blob(['file content'], { type: 'text/plain' });
+    const mockError = new Error('File reading failed');
+
+    jest.spyOn(FileReader.prototype, 'readAsDataURL').mockImplementation(function () {
+      this.onerror(mockError);
+    });
+
+    await expect(fileToDataUri(mockFile)).rejects.toThrow('File reading failed');
+  });
+});
