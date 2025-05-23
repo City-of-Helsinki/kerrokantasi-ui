fbfc43ec513416a5d617e6caca91c193a05b545e
Arto Pärssinen <arto.parssinen@gofore.com>
2025-05-23 12:17:36 +0300

    feat: notification manager implementation

--- src/utils/notificationService.js
+++ src/utils/notificationService.js
@@ -1,23 +1,22 @@
-import api from "../api";
-
-export async function getNotifications(lang) {
-    return api.get("/v1/notifications").then(async response => {
-        const data = await response.json();
-        const notifications = mapNotifications(data, lang);
-        return notifications;
-    });
-}
+import { get } from "../api";
 
 function mapNotifications(notifications, language) {
-    return notifications.map(notification => {
-        return {
+    return notifications.map(notification => ({
             id: notification.id,
-            content: notification[`content_${language}`],
-            title: notification[`title_${language}`],
+            content: notification.content[language],
+            title: notification.title[language],
             level: notification.type_name.toLowerCase(),
             modified_at: notification.modified_at,
-            external_url: notification[`external_url_${language}`],
-            external_url_title: notification[`external_url_title_${language}`],
-        };
+            external_url: notification.external_url[language],
+            external_url_title: notification.external_url_title[language],
+        }));
+}
+
+export async function getNotifications(lang) {
+    return get("/v1/notifications").then(async response => {
+        const data = await response.json();
+        return mapNotifications(data, lang);
     });
 }
+
+export default getNotifications;
