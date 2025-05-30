9393b82b6e949fe9a456b1d0e146831207f6fe1d
Arto Pärssinen <arto.parssinen@gofore.com>
2025-05-30 09:26:44 +0300

    feat: logic for notification display based on location

--- src/components/MaintenanceNotification.jsx
+++ src/components/MaintenanceNotification.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { NotificationService } from 'react-helsinki-notification-manager';
 // eslint-disable-next-line import/no-unresolved
 import 'react-helsinki-notification-manager/style.css';
@@ -7,18 +8,34 @@ import 'react-helsinki-notification-manager/style.css';
 import { getNotifications } from '../utils/notificationService';
 
 const MaintenanceNotification = (props) => {
+  const location = useLocation();
   const [notifications, setNotifications] = useState([]);
-  const { language } = props
+  const { language } = props;
   
+  const visibleTypeRules = useMemo(() => location.pathname !== '/' ?
+    'error' :
+    'all', 
+  [location.pathname]);
+  
+
+
   useEffect(() => {
     const loadNotifications = async () => {
       setNotifications(await getNotifications(language));
     }
     if (language) loadNotifications();
   }, [language]);
+
+  const visibleTypes = useMemo(() => {
+    if (visibleTypeRules === 'all') {
+      return ['error', 'warning', 'info'];
+    }
+    return [visibleTypeRules];
+  }, [visibleTypeRules]);
+
   return (
     <div className='container maintenance-notification-container'>
-      <NotificationService notifications={notifications} />
+      <NotificationService notifications={notifications} visibleTypes={visibleTypes} />
     </div>
   );
 };
