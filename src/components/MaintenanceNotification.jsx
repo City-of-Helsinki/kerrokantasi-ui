264b5d9a7a96ee97610100dd476b945184386816
Arto Pärssinen <62537815+arpargo@users.noreply.github.com>
2025-08-13 15:01:00 +0300

    fix: refactored intl provider to be at the root (#1167)
    * fix: refactored intl provider to be at the root
    
    * chore: removed console debugs

--- src/components/MaintenanceNotification.jsx
+++ src/components/MaintenanceNotification.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { NotificationService } from 'react-helsinki-notification-manager';
 // eslint-disable-next-line import/no-unresolved
 import 'react-helsinki-notification-manager/style.css';
 
 import useNotifications from '../hooks/useNotifications';
+import LoadSpinner from './LoadSpinner';
 
 const MaintenanceNotification = ({ language }) => {
   const { notifications, visibleTypes } = useNotifications(language);
 
   return (
-    <div className='container maintenance-notification-container'>
-      <NotificationService 
-        notifications={notifications} 
-        visibleTypes={visibleTypes}
-        language={language}
-      />
-    </div>
+    <Suspense fallback={<div className='container maintenance-notification-container'><LoadSpinner /></div>}>
+      <div className='container maintenance-notification-container'>
+        <NotificationService 
+          notifications={notifications} 
+          visibleTypes={visibleTypes}
+          language={language}
+        />
+      </div>
+    </Suspense>
   );
 };
 
