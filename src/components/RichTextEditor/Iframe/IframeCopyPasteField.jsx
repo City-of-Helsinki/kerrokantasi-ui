d5eefcdec6516b897cc5b832463502fbed20af7c
mikkojamG <mikko.jamia@gofore.com>
2026-06-17 13:48:56 +0300

    refactor: remaining class components to functions Refs: KER-571

--- src/components/RichTextEditor/Iframe/IframeCopyPasteField.jsx
+++ src/components/RichTextEditor/Iframe/IframeCopyPasteField.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import getMessage from '../../../utils/getMessage';
@@ -7,43 +7,32 @@ import {
   convertStyleDimensionSettings,
 } from '../../../utils/iframeUtils';
 
-class IframeCopyPasteField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      htmlCopyPaste: '',
-    };
+const IframeCopyPasteField = ({ updateAttributes }) => {
+  const [htmlCopyPaste, setHtmlCopyPaste] = useState('');
 
-    this.handleCopyPasteChange = this.handleCopyPasteChange.bind(this);
-  }
-
-  handleCopyPasteChange(event) {
+  const handleCopyPasteChange = (event) => {
     const { value } = event.target;
-    this.setState({ htmlCopyPaste: value });
-
+    setHtmlCopyPaste(value);
     // if iframe has width and/or height in the style attribute
     // convert the dimensions into their own attributes
-    const attributes = convertStyleDimensionSettings(parseIframeHtml(value));
-    this.props.updateAttributes(attributes);
-  }
+    updateAttributes(convertStyleDimensionSettings(parseIframeHtml(value)));
+  };
 
-  render() {
-    return (
-      <div className='input-container html-copy-paste-input'>
-        <label htmlFor='iframe-html-copy-paste'>
-          {getMessage('iframeHtmlCopyPaste')}
-        </label>
-        <textarea
-          id='iframe-html-copy-paste'
-          name='htmlCopyPaste'
-          className='form-control'
-          onChange={this.handleCopyPasteChange}
-          value={this.state.htmlCopyPaste}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='input-container html-copy-paste-input'>
+      <label htmlFor='iframe-html-copy-paste'>
+        {getMessage('iframeHtmlCopyPaste')}
+      </label>
+      <textarea
+        id='iframe-html-copy-paste'
+        name='htmlCopyPaste'
+        className='form-control'
+        onChange={handleCopyPasteChange}
+        value={htmlCopyPaste}
+      />
+    </div>
+  );
+};
 
 IframeCopyPasteField.propTypes = {
   updateAttributes: PropTypes.func,
--- src/components/admin/QuestionForm.jsx
+++ src/components/admin/QuestionForm.jsx
@@ -10,159 +10,133 @@ import MultiLanguageTextField, {
 import Icon from '../../utils/Icon';
 import getAttr from '../../utils/getAttr';
 
-class QuestionForm extends React.Component {
-  /**
-   * Delete an existing question from a section
-   */
-  handleDeleteExistingQuestion = () => {
-    const { question, sectionId } = this.props;
+const QuestionForm = ({
+  question,
+  sectionId,
+  addOption,
+  deleteOption,
+  sectionLanguages,
+  onQuestionChange,
+  onDeleteExistingQuestion,
+  lang,
+  isPublic,
+}) => {
+  const handleDeleteExistingQuestion = () => {
     if (question.id && sectionId) {
-      this.props.onDeleteExistingQuestion(sectionId, question.id);
+      onDeleteExistingQuestion(sectionId, question.id);
     }
   };
 
-  /**
-   * Returns editable form for questions
-   * @returns {JSX.Element}
-   */
-  getEditableForm = () => {
-    const {
-      question,
-      sectionId,
-      addOption,
-      deleteOption,
-      sectionLanguages,
-      onQuestionChange,
-    } = this.props;
-    return (
-      <div className='question-form' key={question.type}>
-        <MultiLanguageTextField
-          labelId=' '
-          maxLength={1000}
-          name='abstract'
-          onBlur={(value) =>
-            onQuestionChange(
-              'text',
-              sectionId,
-              question.frontId || question.id,
-              '',
-              value
-            )
-          }
-          value={question.text}
-          languages={sectionLanguages}
-          fieldType={TextFieldTypes.TEXTAREA}
-          placeholderId='questionTextPlaceholder'
-        />
-        {question.options.map((option, index) => (
-          <div style={{ display: 'flex' }} key={`option-${index}`}>
-            <div style={{ flex: '19' }}>
-              <MultiLanguageTextField
-                labelId='option'
-                name={`content-${index}`}
-                onBlur={(value) =>
-                  onQuestionChange(
-                    'option',
-                    sectionId,
-                    question.frontId || question.id,
-                    index,
-                    value
-                  )
-                }
-                rows='10'
-                value={option.text || {}}
-                languages={sectionLanguages}
-                placeholderId='questionOptionPlaceholder'
-              />
-            </div>
-            <div style={{ flex: '1', marginTop: '48px', marginLeft: '15px' }}>
-              {question.options.length > 2 &&
-                index === question.options.length - 1 && (
-                  <Button
-                    className={'kerrokantasi-btn danger'}
-                    onClick={() =>
-                      deleteOption(
-                        sectionId,
-                        question.frontId || question.id,
-                        index
-                      )
-                    }
-                  >
-                    <Icon
-                      style={{ fontSize: '24px' }}
-                      className='icon'
-                      name='trash'
-                    />
-                  </Button>
-                )}
-            </div>
-          </div>
-        ))}
-        <div>
-          <Button
-            className={'kerrokantasi-btn'}
-            onClick={() =>
-              addOption(sectionId, question.frontId || question.id)
-            }
-          >
-            <Icon className='icon' name='plus' />{' '}
-            <FormattedMessage id='addOption' />
-          </Button>
-        </div>
-      </div>
-    );
-  };
-
-  /**
-   * Returns details of existing question
-   * @returns {JSX.Element}
-   */
-  getQuestionDetails = () => {
-    const { question, lang } = this.props;
-    return (
-      <div>
-        <h6>{getAttr(question.text, lang)}</h6>
-        {
-          <div>
-            {question.options.map((option) => {
-              const answerPercentage =
-                Math.round((option.n_answers / question.n_answers) * 100) || 0;
-              return (
-                <div key={`${question.id}-${option.id}`}>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <div style={{ color: 'blue', margin: 'auto 10px auto 0' }}>
-                      {answerPercentage}%
-                    </div>
-                    <div>{getAttr(option.text, lang)}</div>
-                  </div>
-                  <ProgressBar now={answerPercentage} />
-                </div>
-              );
-            })}
-            <Button
-              className={'kerrokantasi-btn danger'}
-              onClick={this.handleDeleteExistingQuestion}
-            >
-              <FormattedMessage id='deleteQuestion' />
-            </Button>
-          </div>
+  const getEditableForm = () => (
+    <div className='question-form' key={question.type}>
+      <MultiLanguageTextField
+        labelId=' '
+        maxLength={1000}
+        name='abstract'
+        onBlur={(value) =>
+          onQuestionChange(
+            'text',
+            sectionId,
+            question.frontId || question.id,
+            '',
+            value
+          )
         }
+        value={question.text}
+        languages={sectionLanguages}
+        fieldType={TextFieldTypes.TEXTAREA}
+        placeholderId='questionTextPlaceholder'
+      />
+      {question.options.map((option, index) => (
+        <div style={{ display: 'flex' }} key={`option-${index}`}>
+          <div style={{ flex: '19' }}>
+            <MultiLanguageTextField
+              labelId='option'
+              name={`content-${index}`}
+              onBlur={(value) =>
+                onQuestionChange(
+                  'option',
+                  sectionId,
+                  question.frontId || question.id,
+                  index,
+                  value
+                )
+              }
+              rows='10'
+              value={option.text || {}}
+              languages={sectionLanguages}
+              placeholderId='questionOptionPlaceholder'
+            />
+          </div>
+          <div style={{ flex: '1', marginTop: '48px', marginLeft: '15px' }}>
+            {question.options.length > 2 &&
+              index === question.options.length - 1 && (
+                <Button
+                  className={'kerrokantasi-btn danger'}
+                  onClick={() =>
+                    deleteOption(
+                      sectionId,
+                      question.frontId || question.id,
+                      index
+                    )
+                  }
+                >
+                  <Icon
+                    style={{ fontSize: '24px' }}
+                    className='icon'
+                    name='trash'
+                  />
+                </Button>
+              )}
+          </div>
+        </div>
+      ))}
+      <div>
+        <Button
+          className={'kerrokantasi-btn'}
+          onClick={() => addOption(sectionId, question.frontId || question.id)}
+        >
+          <Icon className='icon' name='plus' />{' '}
+          <FormattedMessage id='addOption' />
+        </Button>
       </div>
-    );
-  };
+    </div>
+  );
 
-  render() {
-    const { question, isPublic } = this.props;
-    /**
-     * Display editable form when question is new or when the hearing is not yet public/hasn't been public.
-     * Otherwise display details of existing questions
-     */
-    return question.frontId ||
-      (question.id && !isPublic && question.n_answers === 0)
-      ? this.getEditableForm()
-      : this.getQuestionDetails();
-  }
-}
+  const getQuestionDetails = () => (
+    <div>
+      <h6>{getAttr(question.text, lang)}</h6>
+      <div>
+        {question.options.map((option) => {
+          const answerPercentage =
+            Math.round((option.n_answers / question.n_answers) * 100) || 0;
+          return (
+            <div key={`${question.id}-${option.id}`}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ color: 'blue', margin: 'auto 10px auto 0' }}>
+                  {answerPercentage}%
+                </div>
+                <div>{getAttr(option.text, lang)}</div>
+              </div>
+              <ProgressBar now={answerPercentage} />
+            </div>
+          );
+        })}
+        <Button
+          className={'kerrokantasi-btn danger'}
+          onClick={handleDeleteExistingQuestion}
+        >
+          <FormattedMessage id='deleteQuestion' />
+        </Button>
+      </div>
+    </div>
+  );
+
+  return question.frontId ||
+    (question.id && !isPublic && question.n_answers === 0)
+    ? getEditableForm()
+    : getQuestionDetails();
+};
 
 QuestionForm.propTypes = {
   question: PropTypes.object,
